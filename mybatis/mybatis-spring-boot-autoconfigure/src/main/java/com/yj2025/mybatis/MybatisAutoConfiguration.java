836a60c9d884ad6c8c931c50d7fb7107f3377f4f
serv <yifeng2000@126.com>
2021-05-07 17:48:53 +0800

    租户限制

--- mybatis/mybatis-spring-boot-autoconfigure/src/main/java/com/yj2025/mybatis/MybatisAutoConfiguration.java
+++ mybatis/mybatis-spring-boot-autoconfigure/src/main/java/com/yj2025/mybatis/MybatisAutoConfiguration.java
@@ -1,22 +1,59 @@
 package com.yj2025.mybatis;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.LongValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(TenantConfig.class)
 public class MybatisAutoConfiguration {
+
+    @Autowired
+    private TenantConfig tenantConfig;
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        TenantLineHandler tenantLineHandler = new TenantLineHandler() {
+            @Override
+            public Expression getTenantId() {
+                return new LongValue(1);
+            }
+
+            @Override
+            public String getTenantIdColumn() {
+                return tenantConfig.getField();
+            }
+
+            @Override
+            public boolean ignoreTable(String tableName) {
+                return tenantConfig.getIgnores().contains(tableName);
+            }
+        };
+
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        if (tenantConfig.isEnable()) {
+            interceptor.addInnerInterceptor(new TenantLineInnerInterceptor(tenantLineHandler));
+        }
         // https://baomidou.com/guide/interceptor-optimistic-locker.html#optimisticlockerinnerinterceptor
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
     }
 
 }
---
+++ mybatis/mybatis-spring-boot-autoconfigure/src/main/java/com/yj2025/mybatis/TenantConfig.java
@@ -0,0 +1,47 @@
+package com.yj2025.mybatis;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@ConfigurationProperties(prefix = "druid.data-source")
+public class TenantConfig implements Serializable {
+
+    //默认开启租户模式
+    private boolean enable = true;
+
+    //租户模式下统一的字段名
+    private String field = "ent_code";
+
+    //开启租户模式后,需要忽略的表名
+    private List<String> ignores = new ArrayList<String>();
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public List<String> getIgnores() {
+        if (ignores == null) {
+            ignores = new ArrayList<>();
+        }
+        return ignores;
+    }
+
+    public void setIgnores(List<String> ignores) {
+        this.ignores = ignores;
+    }
+}
