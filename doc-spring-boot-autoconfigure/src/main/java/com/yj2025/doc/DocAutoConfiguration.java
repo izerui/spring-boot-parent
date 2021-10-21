872e7fa5646311da04a6a80eb41dfad1b18c4334
liuyuhua <40409439@qq.com>
2021-10-21 16:54:04 +0800

    租户 兼容类似 `ent_code` 引号的

--- doc-spring-boot-autoconfigure/src/main/java/com/yj2025/doc/DocAutoConfiguration.java
+++ doc-spring-boot-autoconfigure/src/main/java/com/yj2025/doc/DocAutoConfiguration.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +33,7 @@ import java.util.List;
 @Slf4j
 @EnableSwagger2
 @Configuration
+@ConditionalOnWebApplication
 public class DocAutoConfiguration implements WebMvcConfigurer {
 
     @Value("${spring.application.name:null}")
