cd6ab4c172f70c3bd150473de57dc7b910540e10
liuyuhua <40409439@qq.com>
2021-10-28 13:36:16 +0800

    audit 修改

--- lock-spring-boot-sample/src/main/java/com/yj2025/lock/sample/Application.java
+++ lock-spring-boot-sample/src/main/java/com/yj2025/lock/sample/Application.java
@@ -1,7 +1,5 @@
 package com.yj2025.lock.sample;
 
-import com.google.common.collect.Lists;
-import com.yj2025.commons.util.ThreadPoolExecutors;
 import com.yj2025.lock.Lock;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.Assert;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 @Slf4j
 @SpringBootApplication
@@ -31,8 +24,7 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        difThreads();
-        sameParentThreads();
+        difThreads();
         new CountDownLatch(1).await();
     }
 
@@ -53,30 +45,6 @@ public class Application implements CommandLineRunner {
             }).start();
         }
 
-    }
-
-    private void sameParentThreads() throws InterruptedException {
-        AtomicInteger count = new AtomicInteger();
-        List<Integer> integers = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            integers.add(i);
-        }
-
-        ThreadPoolExecutors.newInstance().execute(integers.stream().map(integer -> {
-            Runnable runnable = () -> {
-                while (true) {
-                    lock.execute("temp-2021-08-25", () -> {
-                        log.info(count.get() + "");
-                        Assert.state(count.get() == 0, "锁失败，值冲突");
-                        count.set(1);
-                        Thread.sleep(new Random().nextInt(3) * 100);
-                        count.set(0);
-                        return null;
-                    });
-                }
-            };
-            return runnable;
-        }).collect(Collectors.toList()), 5, 10);
     }
 
 }
