150e57b67802dfc0980f9972e3c87c74919f8bac
liuyuhua <40409439@qq.com>
2021-11-19 18:52:52 +0800

    profile is null case

--- doc-spring-boot-autoconfigure/src/main/java/com/yj2025/doc/LabelCommandLineRunner.java
+++ doc-spring-boot-autoconfigure/src/main/java/com/yj2025/doc/LabelCommandLineRunner.java
@@ -19,6 +19,7 @@ public class LabelCommandLineRunner implements CommandLineRunner {
                     "Application '${application}' is running! Profile Active:[${profile}] Access URLs:\n\t" +
                     "Web: \t\thttp://${host}:${port}${contextPath}\n\t" +
                     "Docs: \t\thttp://${host}:${port}${contextPath}/api\n\t" +
+                    "KoTime: \t\thttp://${host}:${port}${contextPath}/koTime\n\t" +
                     "Actuator: \thttp://${host}:${port}${contextPath}/actuator\n" +
                     "----------------------------------------------------------";
 
