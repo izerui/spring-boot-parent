182f8b822181b9cac8f22ccd16ed9ec18168c4f4
liuyuhua <40409439@qq.com>
2021-11-25 18:35:31 +0800

    修改扩展方式

--- customizer-spring-boot-autoconfigure/src/main/java/com/yj2025/customizer/bean/BeanDefinitionRegistryCustomizer.java
+++ customizer-spring-boot-autoconfigure/src/main/java/com/yj2025/customizer/bean/BeanDefinitionRegistryCustomizer.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.ApplicationContext;
 
 /**
- * 自定义的bean扩展基类，实现了该类的实例，并且注册为spring bean，则会自动根据返回的 BeanDefinitionContext 进行相应的bean定义的覆盖
+ * 自定义的bean扩展基类，实现了该类的实例，并且注册为spring bean，并暴露 BeanDefinitionRegistry 钩子
  */
 @FunctionalInterface
 public interface BeanDefinitionRegistryCustomizer {
