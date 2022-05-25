ea7679c9a46f79ccabb6fe314aa3107bf1cc7c0d
izerui <liuyuhua69@gmail.com>
2022-05-25 23:50:22 +0800

    throws Exception

--- audit-spring-boot/audit-spring-boot-sample/src/main/java/com/yj2025/audit/sample/listener/AuditListener.java
+++ audit-spring-boot/audit-spring-boot-sample/src/main/java/com/yj2025/audit/sample/listener/AuditListener.java
@@ -2,6 +2,7 @@ package com.yj2025.audit.sample.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
