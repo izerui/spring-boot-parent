4a65b495e3d6c484ef757105e99f542232fc78fd
serv <liuyuhua69@gmail.com>
2022-06-02 19:35:46 +0800

    微信封装重构 后 升级版本

--- weixin-spring-boot/work-weixin-spring-boot-autoconfigure/src/main/java/com/yj2025/weixin/work/listener/WxCpListenerController.java
+++ weixin-spring-boot/work-weixin-spring-boot-autoconfigure/src/main/java/com/yj2025/weixin/work/listener/WxCpListenerController.java
@@ -45,31 +45,37 @@ public class WxCpListenerController implements CommandLineRunner {
                           @RequestParam("timestamp") String timestamp,
                           @RequestParam(value = "echostr", required = false) String echostr,
                           HttpServletRequest request) throws IOException {
-        // 必要: 切换配置
-        tenantWxCpService.tenant(tenantId);
-        // 获取切换后的配置存储对象
-        WxCpConfigStorage wxCpConfigStorage = tenantWxCpService.getWxCpConfigStorage();
-        if (StringUtils.isNotBlank(echostr)) {
-            if (!this.tenantWxCpService.checkSignature(msgSignature, timestamp, nonce, echostr)) {
-                // 消息签名不正确，说明不是公众平台发过来的消息
-                return "非法请求";
+        try {
+            // 必要: 切换配置
+            tenantWxCpService.tenant(tenantId);
+            // 获取切换后的配置存储对象
+            WxCpConfigStorage wxCpConfigStorage = tenantWxCpService.getWxCpConfigStorage();
+            if (StringUtils.isNotBlank(echostr)) {
+                if (!this.tenantWxCpService.checkSignature(msgSignature, timestamp, nonce, echostr)) {
+                    // 消息签名不正确，说明不是公众平台发过来的消息
+                    return "非法请求";
+                }
+                WxCpCryptUtil cryptUtil = new WxCpCryptUtil(wxCpConfigStorage);
+                String plainText = cryptUtil.decrypt(echostr);
+                // 说明是一个仅仅用来验证的请求，回显echostr
+                return plainText;
             }
-            WxCpCryptUtil cryptUtil = new WxCpCryptUtil(wxCpConfigStorage);
-            String plainText = cryptUtil.decrypt(echostr);
-            // 说明是一个仅仅用来验证的请求，回显echostr
-            return plainText;
-        }
 
-        Map<String, Object> context = new HashMap<>();
-        context.put("tenantId", tenantId);
+            Map<String, Object> context = new HashMap<>();
+            context.put("tenantId", tenantId);
 
-        WxCpXmlMessage inMessage = WxCpXmlMessage
-                .fromEncryptedXml(request.getInputStream(), wxCpConfigStorage, timestamp, nonce, msgSignature);
-        WxCpXmlOutMessage outMessage = this.wxCpMessageRouter.route(inMessage, context);
-        if (outMessage != null) {
-            return outMessage.toEncryptedXml(wxCpConfigStorage);
+            WxCpXmlMessage inMessage = WxCpXmlMessage
+                    .fromEncryptedXml(request.getInputStream(), wxCpConfigStorage, timestamp, nonce, msgSignature);
+            WxCpXmlOutMessage outMessage = this.wxCpMessageRouter.route(inMessage, context);
+            if (outMessage != null) {
+                return outMessage.toEncryptedXml(wxCpConfigStorage);
+            }
+            return "非法请求";
+        } catch (Exception ex) {
+            log.error("request: {}", request.getQueryString());
+            log.error(ex.getMessage(), ex);
+            return "非法请求";
         }
-        return "非法请求";
     }
 
 
