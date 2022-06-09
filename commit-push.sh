f3e35aacad8e858e3886f811c53bb7f0a03399b4
serv <liuyuhua69@gmail.com>
2022-06-09 15:59:10 +0800

    fixed

--- commit-push.sh
+++ commit-push.sh
@@ -1,6 +1,6 @@
 echo;
-echo '====================== 开始提交变更 ======================'
-echo '开始添加变更：git add .'
+echo '开始提交变更'
+echo '添加变更：git add .'
 git add .
 echo -n "提交注释:"
 read  message
@@ -9,5 +9,5 @@ echo '更新远程代码：git pull'
 git pull
 echo '提交代码到远程：git push'
 git push
-echo '====================== 提交完毕 ======================'
+echo '提交完毕'
 echo;
