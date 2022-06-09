26c814af3faee4d299b366d471f710002f864a5c
serv <liuyuhua69@gmail.com>
2022-06-09 15:53:47 +0800

    收到否

--- commit-push.sh
+++ commit-push.sh
@@ -1,21 +1,20 @@
 echo;
 echo '====================== 开始提交变更 ======================'
-echo `当前目录是：pwd`
-echo;
-
-echo 开始添加变更：git add .
+echo '开始添加变更：git add .'
 git add .
 echo;
 
-set /p declation=输入提交的commit信息:
-git commit -m "%declation%"
+echo -n "提交注释:"
+read  message
+
+git commit -m $message
 echo;
 
-echo 更新远程代码：git pull
+echo '更新远程代码：git pull'
 git pull
 echo;
 
-echo 将变更情况提交到远程分支：git push
+echo '提交代码到远程：git push'
 git push
 echo;
 
