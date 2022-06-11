5cd05b0b31dbde396be4637fdb8f43cbb4760409
serv <liuyuhua69@gmail.com>
2022-06-11 16:15:46 +0800

    升级 1.5.0-SNAPSHOT 增加mongo-transaction 事务支持

--- weixin-spring-boot/work-weixin-spring-boot-sample/src/main/java/com/yj2025/weixin/work22/ureport/UreportConfiguration.java
+++ weixin-spring-boot/work-weixin-spring-boot-sample/src/main/java/com/yj2025/weixin/work22/ureport/UreportConfiguration.java
@@ -1,16 +1,10 @@
 package com.yj2025.weixin.work22.ureport;
 
 import com.bstek.ureport.console.UReportServlet;
-import com.bstek.ureport.definition.datasource.BuildinDatasource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.jdbc.datasource.DataSourceUtils;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
 
 @ImportResource("classpath:ureport-console-context.xml")
 @Configuration
