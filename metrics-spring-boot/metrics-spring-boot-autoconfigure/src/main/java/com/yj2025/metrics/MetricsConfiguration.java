bb0a7c146cb98bde887b5afe0d7cdc6d4e54dc86
serv <liuyuhua69@gmail.com>
2022-06-21 10:47:24 +0800

    1.5.1-SNAPSHOT

--- metrics-spring-boot/metrics-spring-boot-autoconfigure/src/main/java/com/yj2025/metrics/MetricsConfiguration.java
+++ metrics-spring-boot/metrics-spring-boot-autoconfigure/src/main/java/com/yj2025/metrics/MetricsConfiguration.java
@@ -1,12 +1,6 @@
 package com.yj2025.metrics;
 
-import org.apache.skywalking.apm.meter.micrometer.SkywalkingConfig;
-import org.apache.skywalking.apm.meter.micrometer.SkywalkingMeterRegistry;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
 
 @Configuration
 public class MetricsConfiguration {
