7964231b7824a3462d5b1af3ad8a4f6d9e3af34b
serv <yifeng2000@126.com>
2022-06-30 19:03:16 +0800

    tryWith

--- data-jdbc-spring-boot/data-jdbc-spring-boot-sample/src/main/java/com/yj2025/sample/repository/TestUserRepository.java
+++ data-jdbc-spring-boot/data-jdbc-spring-boot-sample/src/main/java/com/yj2025/sample/repository/TestUserRepository.java
@@ -1,10 +1,9 @@
 package com.yj2025.sample.repository;
 
 import com.yj2025.sample.entity.TestUser;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 
-public interface TestUserRepository extends PagingAndSortingRepository<TestUser, Long>, QuerydslPredicateExecutor<TestUser> {
+public interface TestUserRepository extends PagingAndSortingRepository<TestUser, Long> {
 
 }
