0d1eb86019051eaf3af1b7b7b8166549c1e99ac1
serv <yifeng2000@126.com>
2022-07-12 15:03:44 +0800

    cmd相互调用使用 桥接

---
+++ basic-spring-boot/basic-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/command/adapter/BasicAdapter.java
@@ -0,0 +1,13 @@
+package com.yj2025.basic.command.adapter;
+
+import com.yj2025.basic.command.BasicCommand;
+
+public abstract class BasicAdapter<R> extends BasicCommand<R> {
+
+    protected abstract BasicCommand<R> withCommand();
+
+    @Override
+    protected final R doExecute() throws Exception {
+        return withCommand().execute();
+    }
+}
