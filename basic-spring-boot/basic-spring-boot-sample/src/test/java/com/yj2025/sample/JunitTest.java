165e3fc6785060b4521c29274a766c0cb062d1c5
serv <yifeng2000@126.com>
2022-07-12 19:56:14 +0800

    cmd相互调用使用 桥接

--- basic-spring-boot/basic-spring-boot-sample/src/test/java/com/yj2025/sample/JunitTest.java
+++ basic-spring-boot/basic-spring-boot-sample/src/test/java/com/yj2025/sample/JunitTest.java
@@ -3,6 +3,13 @@ package com.yj2025.sample;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yj2025.basic.support.Context;
 import org.junit.Test;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternUtils;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
 
 import java.io.File;
 import java.util.Map;
@@ -15,4 +22,21 @@ public class JunitTest {
             new ObjectMapper().readValue("22ssssss2", Map.class);
         });
     }
+
+    @Test
+    public void test02() {
+        Context.tryWith(() -> {
+            DefaultResourceLoader defaultResourceLoader = new DefaultResourceLoader();
+            ResourcePatternResolver resolver = ResourcePatternUtils.getResourcePatternResolver(defaultResourceLoader);
+            MetadataReaderFactory metaReader = new CachingMetadataReaderFactory(defaultResourceLoader);
+            Resource[] resources = resolver.getResources("classpath*:com/yj2025/**/*Cmd.class");
+
+            for (Resource r : resources) {
+                MetadataReader reader = metaReader.getMetadataReader(r);
+                System.out.println(reader.getClassMetadata().getClassName());
+            }
+        });
+    }
+
+
 }
