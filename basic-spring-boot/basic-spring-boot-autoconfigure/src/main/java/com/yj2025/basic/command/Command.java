bb0e1886e1bc7728e9f795bf622ec69185b19923
serv <yifeng2000@126.com>
2022-07-18 09:18:50 +0800

    日志对象

--- basic-spring-boot/basic-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/command/Command.java
+++ basic-spring-boot/basic-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/command/Command.java
@@ -1,7 +1,5 @@
 package com.yj2025.basic.command;
 
-import org.slf4j.Logger;
-
 public interface Command<R> {
 
     /**
@@ -18,10 +16,4 @@ public interface Command<R> {
      * 执行耗时
      */
     Long getTimeMillis();
-
-    /**
-     * 获取日志对象
-     * @return
-     */
-    Logger getLogger();
 }
