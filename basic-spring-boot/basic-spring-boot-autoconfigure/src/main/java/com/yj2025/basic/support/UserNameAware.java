bedc980248ff3936c5b63d9f7b5190edf340e98c
serv <yifeng2000@126.com>
2022-07-22 11:16:01 +0800

    UserNameAware

---
+++ basic-spring-boot/basic-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/support/UserNameAware.java
@@ -0,0 +1,29 @@
+package com.yj2025.basic.support;
+
+import java.util.Map;
+import java.util.function.Function;
+
+public interface UserNameAware {
+
+    ThreadLocal<Map<String, String>> THREAD_LOCAL = new InheritableThreadLocal<>();
+
+    String getUserCode();
+
+    void setUserName(String userName);
+
+    default <T> T wrapUserName(Function<T, String> nameGetter) {
+        Map<String, String> userMap = THREAD_LOCAL.get();
+        if (this.getUserCode() != null) {
+            if (!userMap.containsKey(this.getUserCode())) {
+                String userName = nameGetter.apply((T) this);
+                if (userName != null) {
+                    userMap.put(this.getUserCode(), userName);
+                } else {
+                    userMap.put(this.getUserCode(), null);
+                }
+            }
+            this.setUserName(userMap.get(this.getUserCode()));
+        }
+        return (T) this;
+    }
+}
