bf6a040f64fb18a4a4afec8e4501b77c4e025475
serv <yifeng2000@126.com>
2022-07-26 15:24:43 +0800

    upgrade

--- basic-spring-boot/basic-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/support/UserNameAware.java
+++ basic-spring-boot/basic-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/support/UserNameAware.java
@@ -1,5 +1,6 @@
 package com.yj2025.basic.support;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -13,6 +14,10 @@ public interface UserNameAware {
 
     default <T> T wrapUserName(Function<T, String> nameGetter) {
         Map<String, String> userMap = THREAD_LOCAL.get();
+        if (userMap == null) {
+            userMap = new HashMap<>();
+            THREAD_LOCAL.set(userMap);
+        }
         if (this.getUserCode() != null) {
             if (!userMap.containsKey(this.getUserCode())) {
                 String userName = nameGetter.apply((T) this);
