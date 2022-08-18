3186027ed63dd892ed545bdc09c582213384a330
serv <liuyuhua69@gmail.com>
2022-08-18 19:01:21 +0800

    PageReq

--- basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/request/PageReq.java
+++ basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/request/PageReq.java
@@ -1,7 +1,15 @@
 package com.yj2025.basic.web.request;
 
 
-public class PageReq {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Data
+public abstract class PageReq {
 
     /**
      * 页码
@@ -13,19 +21,29 @@ public class PageReq {
      */
     private Integer pageSize = 20;
 
-    public Integer getPageIndex() {
-        return pageIndex;
+    /**
+     * 排序字段
+     */
+    private String sortField;
+
+    /**
+     * 排序方向
+     */
+    private Sort.Direction sortDirection = Sort.Direction.DESC;
+
+    @JsonIgnore
+    protected abstract Sort getDefaultSort();
+
+    @JsonIgnore
+    public Sort getSort() {
+        if (StringUtils.isNotBlank(sortField)) {
+            return Sort.by(sortDirection, sortField);
+        }
+        return getDefaultSort();
     }
 
-    public void setPageIndex(Integer pageIndex) {
-        this.pageIndex = pageIndex;
-    }
-
-    public Integer getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(Integer pageSize) {
-        this.pageSize = pageSize;
+    @JsonIgnore
+    public Pageable getPageable() {
+        return PageRequest.of(this.pageIndex, this.pageSize, getSort());
     }
 }
