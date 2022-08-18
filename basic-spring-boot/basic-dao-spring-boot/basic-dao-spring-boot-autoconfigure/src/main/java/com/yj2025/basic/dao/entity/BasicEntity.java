bdc4f6e953131a4c085a794e8274f8bd28ee31b2
serv <liuyuhua69@gmail.com>
2022-08-18 15:03:34 +0800

    1

--- basic-spring-boot/basic-dao-spring-boot/basic-dao-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dao/entity/BasicEntity.java
+++ basic-spring-boot/basic-dao-spring-boot/basic-dao-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dao/entity/BasicEntity.java
@@ -3,7 +3,6 @@ package com.yj2025.basic.dao.entity;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -39,33 +38,4 @@ public abstract class BasicEntity {
     @Column(name = "ent_code", columnDefinition = "VARCHAR(64) COMMENT '企业编码'", nullable = false)
     protected String entCode;
 
-    /**
-     * 创建人
-     */
-    @Column(name = "creator", columnDefinition = "VARCHAR(64) COMMENT '创建人'", nullable = false)
-    protected String creator;
-
-    /**
-     * 创建时间
-     */
-    @Column(name = "create_time", columnDefinition = "DATETIME COMMENT '创建时间'", nullable = false)
-    protected Date createTime = new Date();
-
-    /**
-     * 更新人
-     */
-    @Column(name = "updater", columnDefinition = "VARCHAR(64) COMMENT '更新人'")
-    protected String updater;
-
-    /**
-     * 更新时间
-     */
-    @Column(name = "update_time", columnDefinition = "DATETIME COMMENT '更新时间'")
-    protected Date updateTime;
-
-    /**
-     * 删除状态
-     */
-    @Column(name = "deleted", columnDefinition = "bit(1) COMMENT '逻辑删除 0未删除 1删除'", nullable = false)
-    protected boolean deleted = false;
 }
