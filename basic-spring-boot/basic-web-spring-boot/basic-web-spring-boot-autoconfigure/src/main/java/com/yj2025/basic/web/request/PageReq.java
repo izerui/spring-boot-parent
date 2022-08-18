40d8e4f96f83fec5103dd3680ef24cdbffc6eb3b
serv <liuyuhua69@gmail.com>
2022-08-18 19:12:34 +0800

    PageReq1

--- basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/request/PageReq.java
+++ basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/request/PageReq.java
@@ -3,7 +3,7 @@ package com.yj2025.basic.web.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -34,10 +34,29 @@ public abstract class PageReq {
     @JsonIgnore
     protected abstract Sort getDefaultSort();
 
+    /**
+     * 可覆盖更改排序字段名
+     *
+     * @return
+     */
+    protected String[] getSortFields() {
+        return new String[]{sortField};
+    }
+
+
+    /**
+     * 可覆盖更改排序方向字段名
+     * @return
+     */
+    protected Sort.Direction getSortDirection() {
+        return sortDirection;
+    }
+
     @JsonIgnore
     public Sort getSort() {
-        if (StringUtils.isNotBlank(sortField)) {
-            return Sort.by(sortDirection, sortField);
+        String[] sortFields = getSortFields();
+        if (sortFields != null && ArrayUtils.isNotEmpty(sortFields)) {
+            return Sort.by(getSortDirection(), sortFields);
         }
         return getDefaultSort();
     }
