00794626d11b81b9b021a2ebe1c516ff8f4ce4a0
serv <liuyuhua69@gmail.com>
2022-08-19 16:24:56 +0800

    1

---
+++ basic-spring-boot/basic-command-spring-boot/basic-command-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dto/BasicAuthDTO.java
@@ -0,0 +1,21 @@
+package com.yj2025.basic.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public abstract class BasicAuthDTO implements Serializable {
+
+    private Auth auth;
+
+    @Data
+    public static class Auth {
+        private String entCode;
+        private String entName;
+        private String userCode;
+        private String userName;
+        private String accountCode;
+        private String accountName;
+    }
+}
