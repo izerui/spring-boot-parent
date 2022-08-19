ddacbb6b59458c43819fd79d806478f766c54761
serv <liuyuhua69@gmail.com>
2022-08-19 17:33:10 +0800

    11

--- basic-spring-boot/basic-command-spring-boot/basic-command-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dto/BasicAuthDTO.java
+++ basic-spring-boot/basic-command-spring-boot/basic-command-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dto/BasicAuthDTO.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 
 public abstract class BasicAuthDTO implements Serializable {
 
-    private Auth auth;
+    protected Auth auth;
 
     public Auth getAuth() {
         Assert.notNull(auth, "无法获取auth对象,请检查是否赋值");
