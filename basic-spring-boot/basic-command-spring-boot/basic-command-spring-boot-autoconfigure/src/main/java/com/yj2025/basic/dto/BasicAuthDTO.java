da95471f49b49ed3f758d9bc94909862e2f17521
serv <yifeng2000@126.com>
2022-08-20 12:45:04 +0800

    扁平化

--- basic-spring-boot/basic-command-spring-boot/basic-command-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dto/BasicAuthDTO.java
+++ basic-spring-boot/basic-command-spring-boot/basic-command-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dto/BasicAuthDTO.java
@@ -1,30 +1,16 @@
 package com.yj2025.basic.dto;
 
 import lombok.Data;
-import org.springframework.util.Assert;
 
 import java.io.Serializable;
 
+@Data
 public abstract class BasicAuthDTO implements Serializable {
 
-    protected Auth auth;
-
-    public Auth getAuth() {
-        Assert.notNull(auth, "无法获取auth对象,请检查是否赋值");
-        return auth;
-    }
-
-    public void setAuth(Auth auth) {
-        this.auth = auth;
-    }
-
-    @Data
-    public static class Auth {
-        private String entCode;
-        private String entName;
-        private String userCode;
-        private String userName;
-        private String accountCode;
-        private String accountName;
-    }
+    protected String entCode;
+    protected String entName;
+    protected String userCode;
+    protected String userName;
+    protected String accountCode;
+    protected String accountName;
 }
