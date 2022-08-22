73636386d6c46565852eed6611110546dc4fb249
serv <liuyuhua69@gmail.com>
2022-08-22 10:28:00 +0800

    pagerequest 增加validation

--- basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/request/PageReq.java
+++ basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/request/PageReq.java
@@ -9,17 +9,23 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 @Data
 public abstract class PageReq implements AuthAware {
 
     /**
      * 页码
      */
+    @NotNull(message = "页码不能为空,起始页从0开始")
     private Integer pageIndex = 0;
 
     /**
      * 每页条目数
      */
+    @NotNull(message = "每页条目数不能为空")
+    @Max(value = 2000, message = "每页条目数最大不能超过2000条")
     private Integer pageSize = 20;
 
     /**
