cbfc0d0fc913f6471f35626a94e3b1bd3e3f3034
serv <liuyuhua69@gmail.com>
2022-08-22 13:43:08 +0800

    sort

--- basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/request/PageReq.java
+++ basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/request/PageReq.java
@@ -3,13 +3,16 @@ package com.yj2025.basic.web.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.yj2025.basic.web.support.AuthAware;
+import io.vavr.control.Option;
 import lombok.Data;
-import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import javax.annotation.Nullable;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public abstract class PageReq implements AuthAware {
@@ -18,24 +21,20 @@ public abstract class PageReq implements AuthAware {
      * 页码
      */
     @NotNull(message = "页码不能为空,起始页从0开始")
-    private Integer pageIndex = 0;
+    private Integer page = 0;
 
     /**
      * 每页条目数
      */
     @NotNull(message = "每页条目数不能为空")
     @Max(value = 2000, message = "每页条目数最大不能超过2000条")
-    private Integer pageSize = 20;
+    private Integer size = 20;
 
     /**
      * 排序字段
      */
-    private String sortField;
+    List<OrderRequest> orders = new ArrayList<>();
 
-    /**
-     * 排序方向
-     */
-    private Sort.Direction sortDirection = Sort.Direction.DESC;
 
     /**
      * 默认的排序方式
@@ -45,32 +44,29 @@ public abstract class PageReq implements AuthAware {
     protected abstract Sort withDefaultSort();
 
 
-    /**
-     * 可覆盖更改排序字段名
-     *
-     * @return
-     */
-    protected String[] withSortFields() {
-        return new String[]{sortField};
-    }
-
-
-    /**
-     * 可覆盖更改排序方向字段名
-     *
-     * @return
-     */
-    protected Sort.Direction withSortDirection() {
-        return sortDirection;
-    }
-
     @JsonIgnore
     public final PageRequest getPageRequest() {
         Sort sort = withDefaultSort();
-        String[] sortFields = withSortFields();
-        if (sortFields != null && ArrayUtils.isNotEmpty(sortFields)) {
-            sort = Sort.by(withSortDirection(), sortFields);
+        if (orders != null && !orders.isEmpty()) {
+            sort = Sort.by(io.vavr.collection.List.ofAll(orders).map(OrderRequest::toOrder).toJavaList());
         }
-        return PageRequest.of(this.pageIndex, this.pageSize, sort);
+        return PageRequest.of(this.page, this.size, sort);
+    }
+
+    public static class OrderRequest {
+        @Nullable
+        String property;
+        @Nullable
+        Sort.Direction direction;
+
+        @JsonIgnore
+        public Sort.Order toOrder() {
+            return Option.when(property != null, () -> Option
+                    .when(direction == null, Sort.Order.by(property))
+                    .getOrElse(
+                            Option.when(direction.isAscending(), Sort.Order.asc(property))
+                                    .getOrElse(Sort.Order.desc(property))
+                    )).getOrNull();
+        }
     }
 }
