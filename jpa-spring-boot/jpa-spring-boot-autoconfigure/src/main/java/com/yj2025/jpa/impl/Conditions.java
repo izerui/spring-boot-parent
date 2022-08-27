3a10c547373dc30607507ea577c9594d7e072ce5
serv <yifeng2000@126.com>
2022-08-27 18:55:48 +0800

    避免条件不满足时候，执行后面的逻辑块

--- jpa-spring-boot/jpa-spring-boot-autoconfigure/src/main/java/com/yj2025/jpa/impl/Conditions.java
+++ jpa-spring-boot/jpa-spring-boot-autoconfigure/src/main/java/com/yj2025/jpa/impl/Conditions.java
@@ -93,7 +93,18 @@ public class Conditions implements Cloneable {
         return this;
     }
 
+    public Conditions and(boolean valid, Supplier<Conditions> conditions) {
+        if (valid) {
+            this.combList.add(new CombCondition("and", conditions.get()));
+        }
+        return this;
+    }
+
     public Conditions and(Supplier<Boolean> valid, Conditions conditions) {
+        return and(valid.get(), conditions);
+    }
+
+    public Conditions and(Supplier<Boolean> valid, Supplier<Conditions> conditions) {
         return and(valid.get(), conditions);
     }
 
@@ -109,7 +120,18 @@ public class Conditions implements Cloneable {
         return this;
     }
 
+    public Conditions or(boolean valid, Supplier<Conditions> conditions) {
+        if (valid) {
+            this.combList.add(new CombCondition("or", conditions.get()));
+        }
+        return this;
+    }
+
     public Conditions or(Supplier<Boolean> valid, Conditions conditions) {
+        return or(valid.get(), conditions);
+    }
+
+    public Conditions or(Supplier<Boolean> valid, Supplier<Conditions> conditions) {
         return or(valid.get(), conditions);
     }
 
