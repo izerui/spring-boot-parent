0a845d2574681b28fce43a17e25bceb2dfdc8c37
serv <yifeng2000@126.com>
2022-08-27 12:21:52 +0800

    pageIndex

--- basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/request/PageReq.java
+++ basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/request/PageReq.java
@@ -21,14 +21,14 @@ public abstract class PageReq implements AuthAware {
      * 页码
      */
     @NotNull(message = "页码不能为空,起始页从0开始")
-    private Integer page = 0;
+    private Integer pageIndex = 0;
 
     /**
      * 每页条目数
      */
     @NotNull(message = "每页条目数不能为空")
     @Max(value = 2000, message = "每页条目数最大不能超过2000条")
-    private Integer size = 20;
+    private Integer pageSize = 20;
 
     /**
      * 排序字段
@@ -50,7 +50,7 @@ public abstract class PageReq implements AuthAware {
         if (orders != null && !orders.isEmpty()) {
             sort = Sort.by(io.vavr.collection.List.ofAll(orders).map(OrderRequest::toOrder).toJavaList());
         }
-        return PageRequest.of(this.page, this.size, sort);
+        return PageRequest.of(this.pageIndex, this.pageSize, sort);
     }
 
     public static class OrderRequest {
