e8e542daceb3aaf48b72f2c2c9f03d3cafb3aa98
serv <liuyuhua69@gmail.com>
2022-09-12 14:34:02 +0800

    修改key 统一为 fileId名字

--- platform-commons/src/main/java/com/yj2025/commons/vo/AttachmentVO.java
+++ platform-commons/src/main/java/com/yj2025/commons/vo/AttachmentVO.java
@@ -8,18 +8,55 @@ import java.io.Serializable;
 public class AttachmentVO implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 文件id
+     */
     private String fileId;
+    /**
+     * 文件存储桶
+     */
     private String bucket;
+    /**
+     * 是否是私有桶
+     */
     private Boolean privateBucket;
+    /**
+     * 文件名
+     */
     private String fileName;
+    /**
+     * 文件大小
+     */
     private Double fileSize;
+    /**
+     * 文件类型
+     */
     private String mimeType;
+    /**
+     * 文件后缀
+     */
     private String ext;
+    /**
+     * 文件hash
+     */
     private String hash;
+    /**
+     * 文件预览地址
+     */
     private String previewUrl;
+    /**
+     * 文件下载地址
+     */
     private String downloadUrl;
+    /**
+     * 备注
+     */
     private String remark;
 
+    /**
+     * 同文件预览地址
+     * @return
+     */
     public String getUrl() {
         return previewUrl;
     }
