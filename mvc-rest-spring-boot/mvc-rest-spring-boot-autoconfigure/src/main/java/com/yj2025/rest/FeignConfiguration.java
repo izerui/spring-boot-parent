d6e56da9a2b67b798629666e91614119362e492b
serv <liuyuhua69@gmail.com>
2022-09-13 20:43:14 +0800

    eTag 大小写区分

--- mvc-rest-spring-boot/mvc-rest-spring-boot-autoconfigure/src/main/java/com/yj2025/rest/FeignConfiguration.java
+++ mvc-rest-spring-boot/mvc-rest-spring-boot-autoconfigure/src/main/java/com/yj2025/rest/FeignConfiguration.java
@@ -12,6 +12,12 @@ import org.springframework.cloud.openfeign.encoding.BaseRequestInterceptor;
 import org.springframework.cloud.openfeign.encoding.FeignClientEncodingProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Iterator;
 
 /**
  * Created by serv on 2017/3/1.
@@ -52,10 +58,24 @@ public class FeignConfiguration {
         return new BaseRequestInterceptor(feignClientEncodingProperties) {
             @Override
             public void apply(RequestTemplate template) {
+                wrapHeaders(template);
                 template.header(Constants.CLIENT_TYPE, Constants.FEIGN_REQUEST_TYPE);
                 template.header(Constants.CLIENT_NAME, applicationName);
             }
         };
+    }
+
+    private void wrapHeaders(RequestTemplate template) {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes != null) {
+            HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+            if (request != null) {
+                Iterator<String> headerNameIterator = request.getHeaderNames().asIterator();
+                headerNameIterator.forEachRemaining(headerName -> {
+                    template.header(headerName, request.getHeader(headerName));
+                });
+            }
+        }
     }
 
 }
