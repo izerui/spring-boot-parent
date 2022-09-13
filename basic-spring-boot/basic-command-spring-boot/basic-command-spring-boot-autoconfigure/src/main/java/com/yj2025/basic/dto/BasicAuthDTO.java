b280f226df3b6f24d5c285ac624eded0e28ae4a3
serv <liuyuhua69@gmail.com>
2022-09-13 21:15:56 +0800

    去掉 name强限制不能为空

--- basic-spring-boot/basic-command-spring-boot/basic-command-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dto/BasicAuthDTO.java
+++ basic-spring-boot/basic-command-spring-boot/basic-command-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dto/BasicAuthDTO.java
@@ -13,19 +13,15 @@ public abstract class BasicAuthDTO implements Serializable {
     @NotBlank(message = "账套编号不能为空")
     protected String entCode;
 
-    @NotBlank(message = "账套名称不能为空")
     protected String entName;
 
     @NotBlank(message = "用户编号不能为空")
     protected String userCode;
 
-    @NotBlank(message = "用户名不能为空")
     protected String userName;
 
-    @NotBlank(message = "账号编码不能为空")
     protected String accountCode;
 
-    @NotBlank(message = "账号名不能为空")
     protected String accountName;
 
 }
