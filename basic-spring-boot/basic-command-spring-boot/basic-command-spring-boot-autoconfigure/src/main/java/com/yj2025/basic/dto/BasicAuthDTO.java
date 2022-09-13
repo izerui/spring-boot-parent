8408167e20fd03f8518e1798b9a6af3a478450e5
serv <liuyuhua69@gmail.com>
2022-09-13 16:58:13 +0800

    AuthAware 不再忽略 entCode 这些的json序列化

--- basic-spring-boot/basic-command-spring-boot/basic-command-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dto/BasicAuthDTO.java
+++ basic-spring-boot/basic-command-spring-boot/basic-command-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dto/BasicAuthDTO.java
@@ -1,17 +1,31 @@
 package com.yj2025.basic.dto;
 
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Data
+@Validated
 public abstract class BasicAuthDTO implements Serializable {
 
+    @NotBlank(message = "账套编号不能为空")
     protected String entCode;
+
+    @NotBlank(message = "账套名称不能为空")
     protected String entName;
+
+    @NotBlank(message = "用户编号不能为空")
     protected String userCode;
+
+    @NotBlank(message = "用户名不能为空")
     protected String userName;
+
+    @NotBlank(message = "账号编码不能为空")
     protected String accountCode;
+
+    @NotBlank(message = "账号名不能为空")
     protected String accountName;
 
 }
