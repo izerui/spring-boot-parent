4d32c3d6a2c0d23ab617932ddd9ea5ba6cecc33f
serv <liuyuhua69@gmail.com>
2022-09-20 15:01:08 +0800

    如果audit配置未配置，则不发送audit消息

--- audit-spring-boot/audit-spring-boot-autoconfigure/src/main/java/com/yj2025/audit/RabbitAuditProperties.java
+++ audit-spring-boot/audit-spring-boot-autoconfigure/src/main/java/com/yj2025/audit/RabbitAuditProperties.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 public class RabbitAuditProperties {
 
-    private String exchange = "ierp";
-    private String routingKey = "audit";
+    private String exchange;
+    private String routingKey;
 }
