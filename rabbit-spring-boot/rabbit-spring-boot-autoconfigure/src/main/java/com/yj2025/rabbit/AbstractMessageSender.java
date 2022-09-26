2ef5806dbdf8ff8a05c2856a96fa9bdc11a2c12b
serv <yifeng2000@126.com>
2022-09-26 18:04:28 +0800

    AbstractMessageSender

--- rabbit-spring-boot/rabbit-spring-boot-autoconfigure/src/main/java/com/yj2025/rabbit/AbstractMessageSender.java
+++ rabbit-spring-boot/rabbit-spring-boot-autoconfigure/src/main/java/com/yj2025/rabbit/AbstractMessageSender.java
@@ -1,27 +1,10 @@
 package com.yj2025.rabbit;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 public abstract class AbstractMessageSender implements IMessageRouter {
 
     private RabbitTemplate rabbitTemplate;
-    private final static ObjectMapper OBJECT_MAPPER;
-
-    static {
-        OBJECT_MAPPER = new ObjectMapper();
-        OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        OBJECT_MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        OBJECT_MAPPER.registerModule(new ParameterNamesModule())
-                .registerModule(new Jdk8Module())
-                .registerModule(new JavaTimeModule());
-    }
-
 
     public AbstractMessageSender(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
