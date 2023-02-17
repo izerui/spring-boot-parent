37be5fc1c91adac32e7f1eb5f96211d27f6ec196
cofey <zhangbk@yj2025.com>
2023-02-17 18:52:13 +0800

    增加价税信息计算工具类

---
+++ platform-commons/src/main/java/com/yj2025/commons/util/TaxesUtils.java
@@ -0,0 +1,58 @@
+package com.yj2025.commons.util;
+
+import com.yj2025.commons.em.MoneyEnum;
+import com.yj2025.commons.vo.TaxRateVo;
+
+import java.math.BigDecimal;
+
+public class TaxesUtils {
+    @Deprecated
+    public final static int PRICE = 8;
+    @Deprecated
+    public final static int AMOUNT = 2;
+
+    public TaxesUtils() {
+    }
+
+    /**
+     * 计算价税信息，
+     *
+     * @param moneyEnum     格式化方式
+     * @param originalValue 原始金额
+     * @param taxIncluded   是否含税
+     * @param taxRate       税率
+     * @return
+     */
+    public static TaxRateVo calcTaxes(MoneyEnum moneyEnum, BigDecimal originalValue, Boolean taxIncluded, BigDecimal taxRate) {
+        taxRate = MoneyEnum.TAX_RATE.format(BigDecimalUtils.null2Zero(taxRate));
+        originalValue = moneyEnum.format(BigDecimalUtils.null2Zero(originalValue));
+        int scale = moneyEnum.getDecimal();
+        TaxRateVo vo = new TaxRateVo(originalValue, taxIncluded, taxRate, moneyEnum);
+        if (BigDecimalUtils.eqZero(originalValue) || BigDecimalUtils.eqZero(taxRate) || taxIncluded == null) {
+            return vo;
+        }
+        if (taxIncluded) {
+            //含税算税金
+            //税金 = 总价-（总价÷（1+税率/100））
+            //税金 = 总价-（总价*100÷（100+税率）/100）
+//            BigDecimal noneTaxes = moneyEnum.format(originalValue.divide(BigDecimal.ONE.add(taxRate.divide(BigDecimal.valueOf(100), scale, BigDecimal.ROUND_HALF_UP)), scale, BigDecimal.ROUND_HALF_UP));
+            BigDecimal noneTaxes = originalValue.multiply(BigDecimal.valueOf(100)).divide(BigDecimal.valueOf(100).add(taxRate), scale, BigDecimal.ROUND_HALF_UP);
+            BigDecimal taxes = originalValue.subtract(noneTaxes);
+            vo.setTaxes(taxes);
+            vo.setHasTaxes(moneyEnum.format(originalValue));
+            vo.setNoneTaxes(moneyEnum.format(noneTaxes));
+        } else {
+            //不含税算税金
+            BigDecimal taxes = moneyEnum.format(originalValue.multiply(taxRate.divide(BigDecimal.valueOf(100), scale, BigDecimal.ROUND_HALF_UP)));
+            vo.setTaxes(taxes);
+            vo.setHasTaxes(moneyEnum.format(originalValue.add(taxes)));
+            vo.setNoneTaxes(moneyEnum.format(originalValue));
+        }
+        return vo;
+    }
+
+    public static void main(String[] args) {
+        TaxRateVo taxRateVo = TaxesUtils.calcTaxes(MoneyEnum.AMOUNT, new BigDecimal(100), true, new BigDecimal(6.5));
+        System.out.println(taxRateVo);
+    }
+}
---
+++ platform-commons/src/main/java/com/yj2025/commons/vo/TaxRateVo.java
@@ -0,0 +1,70 @@
+package com.yj2025.commons.vo;
+
+import com.google.common.base.MoreObjects;
+import com.yj2025.commons.em.MoneyEnum;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+public class TaxRateVo implements Serializable {
+
+    /**
+     * 原始数据
+     */
+    private BigDecimal original;
+    /**
+     * 是否含税
+     */
+    private Boolean taxIncluded;
+
+    /**
+     * 转换方式，保留几位小数位
+     */
+    private MoneyEnum moneyEnum;
+
+    /**
+     * 税率
+     */
+    private BigDecimal taxRate;
+    /**
+     * 税金
+     */
+    private BigDecimal taxes;
+
+    /**
+     * 含税数据
+     */
+    private BigDecimal hasTaxes;
+
+    /**
+     * 不含税数据
+     */
+    private BigDecimal noneTaxes;
+
+    public TaxRateVo(BigDecimal originalValue, boolean taxIncluded, BigDecimal taxRate, MoneyEnum moneyEnum) {
+        this.original = originalValue;
+        this.hasTaxes = originalValue;
+        this.noneTaxes = originalValue;
+        this.taxIncluded = taxIncluded;
+        this.taxRate = taxRate;
+        this.taxes = moneyEnum.format(BigDecimal.ZERO);
+        this.moneyEnum = moneyEnum;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("original", original)
+                .add("taxIncluded", taxIncluded)
+                .add("moneyEnum", moneyEnum)
+                .add("taxRate", taxRate)
+                .add("taxes", taxes)
+                .add("hasTaxes", hasTaxes)
+                .add("noneTaxes", noneTaxes)
+                .toString();
+    }
+}
