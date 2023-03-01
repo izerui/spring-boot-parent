53e53ad8a134cb6208dba09a6a5ef9b6f5f0159f
cofey <zhangbk@yj2025.com>
2023-03-01 15:32:03 +0800

    增加版本类型

---
+++ platform-commons/src/main/java/com/yj2025/commons/em/VersionType.java
@@ -0,0 +1,37 @@
+package com.yj2025.commons.em;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public enum VersionType {
+    MANUFACTURE("智造版", Lists.newArrayList("new-dev", "dev", "test", "uat", "yunji")),
+    TRADE("星链版", Lists.newArrayList("ec-dev", "mlf-test", "ec-yunji")),
+    MACHINE_WORK("数控版", Lists.newArrayList("mlf-dev", "mlf-test", "mlf-yunji")),
+    ;
+    private String remark;
+    private List<String> profiles;
+
+    VersionType(String remark, List<String> profiles) {
+        this.remark = remark;
+        this.profiles = profiles;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public List<String> getProfiles() {
+        return profiles;
+    }
+
+    public static VersionType getVersionType(String profiles) {
+        for (VersionType versionType : VersionType.values()) {
+            long count = versionType.getProfiles().stream().filter(v -> v.contains(profiles)).count();
+            if (count > 0) {
+                return versionType;
+            }
+        }
+        return null;
+    }
+}
