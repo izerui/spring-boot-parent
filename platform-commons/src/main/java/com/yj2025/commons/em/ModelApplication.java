b56ee78fccd227ee6b9e1ad7e546ba77119ab64c
cofey <zhangbk@yj2025.com>
2023-03-04 18:18:28 +0800

    增加应用

---
+++ platform-commons/src/main/java/com/yj2025/commons/em/ModelApplication.java
@@ -0,0 +1,18 @@
+package com.yj2025.commons.em;
+
+public enum ModelApplication {
+    NONE("当前版本"),//缺省值，每个版本中默认就对应的是VersionType自己业务
+    ATTENDANCE("考勤系统"),
+    CLOUD_FINANCE("财务系统"),
+    PROOFING("开发打样"),
+    ;
+    private String remark;
+
+    ModelApplication(String remark) {
+        this.remark = remark;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+}
--- platform-commons/src/main/java/com/yj2025/commons/em/VersionType.java
+++ platform-commons/src/main/java/com/yj2025/commons/em/VersionType.java
@@ -8,6 +8,7 @@ public enum VersionType {
     MANUFACTURE("智造版", Lists.newArrayList("new-dev", "dev", "test", "uat", "yunji")),
     TRADE("星链版", Lists.newArrayList("ec-dev", "mlf-test", "ec-yunji")),
     MACHINE_WORK("数控版", Lists.newArrayList("mlf-dev", "mlf-test", "mlf-yunji")),
+    CONNECTION("联通版", List.of()),
     ;
     private String remark;
     private List<String> profiles;
