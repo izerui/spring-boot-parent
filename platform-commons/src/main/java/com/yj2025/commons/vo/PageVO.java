c438c07c1e97e2d78b6d276bd9fadedff53d0436
cofey <zhangbk@yj2025.com>
2023-03-20 16:27:33 +0800

    --

--- platform-commons/src/main/java/com/yj2025/commons/vo/PageVO.java
+++ platform-commons/src/main/java/com/yj2025/commons/vo/PageVO.java
@@ -1,9 +1,7 @@
 package com.yj2025.commons.vo;
 
-import io.vavr.control.Try;
 import lombok.Data;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.function.Function;
@@ -36,65 +34,7 @@ public class PageVO<T> {
         this.hasNext = this.number + 1 < this.getTotalPages();
     }
 
-    public PageVO(List<T> content, Pageable pageable, long total) {
-        this.content = content;
-        this.size = pageable.getPageSize();
-        this.totalElements = total;
-        this.number = pageable.getPageNumber();
-        this.totalPages = getSize() == 0 ? 1 : (int) Math.ceil((double) total / (double) getSize());;
-        this.hasNext = this.number + 1 < this.getTotalPages();
-    }
-
     public PageVO() {
-    }
-
-    /**
-     * 转换成PageVo 保持原对象不变
-     *
-     * @param page
-     * @param <S>
-     * @return
-     */
-    public static <S> PageVO<S> map(Object page) {
-        try {
-            Class<?> pageImplClass = Try.of(() -> Class.forName("org.springframework.data.domain.PageImpl")).getOrNull();
-            Class<?> pageClass = Try.of(() -> Class.forName("com.baomidou.mybatisplus.extension.plugins.pagination.Page")).getOrNull();
-            if (pageImplClass != null && pageImplClass.isAssignableFrom(page.getClass())) {
-                return PageSpringDataVO.map(page);
-            } else if (pageClass != null && pageClass.isAssignableFrom(page.getClass())) {
-                return PageBaomidouVO.map(page);
-            }
-            throw new RuntimeException("不支持");
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            throw new RuntimeException(ex.getMessage(), ex);
-        }
-
-    }
-
-    /**
-     * 转换成PageVo 对象转换成T
-     *
-     * @param page
-     * @param mapper
-     * @param <S>
-     * @param <T>
-     * @return
-     */
-    public static <S, T> PageVO<T> map(Object page, Function<S, T> mapper, Class<S> clazz) {
-        try {
-            Class<?> pageImplClass = Try.of(() -> Class.forName("org.springframework.data.domain.PageImpl")).getOrNull();
-            Class<?> pageClass = Try.of(() -> Class.forName("com.baomidou.mybatisplus.extension.plugins.pagination.Page")).getOrNull();
-            if (pageImplClass != null && pageImplClass.isAssignableFrom(page.getClass())) {
-                return PageSpringDataVO.map(page, mapper, clazz);
-            } else if (pageClass != null && pageClass.isAssignableFrom(page.getClass())) {
-                return PageBaomidouVO.map(page, mapper, clazz);
-            }
-            throw new RuntimeException("未找到jpa或者mybatis-plus的分页类");
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            throw new RuntimeException(ex.getMessage(), ex);
-        }
     }
 
     public static <S, T> PageVO<T> map(Page<S> page, Function<S, T> mapper) {
