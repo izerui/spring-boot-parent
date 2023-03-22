963769e44d132498e67e27872541fa8ca35db79d
cofey <zhangbk@yj2025.com>
2023-03-22 09:09:01 +0800

    -- 消息日志

--- rabbit-spring-boot/rabbit-spring-boot-autoconfigure/src/main/java/com/yj2025/rabbit/AbstractMessageSender.java
+++ rabbit-spring-boot/rabbit-spring-boot-autoconfigure/src/main/java/com/yj2025/rabbit/AbstractMessageSender.java
@@ -1,8 +1,12 @@
 package com.yj2025.rabbit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 public abstract class AbstractMessageSender implements IMessageRouter {
+
+    private static final Logger log = LoggerFactory.getLogger(AbstractMessageSender.class);
 
     private RabbitTemplate rabbitTemplate;
 
@@ -17,9 +21,11 @@ public abstract class AbstractMessageSender implements IMessageRouter {
      */
     public final void sendMessage(Object msg) {
         rabbitTemplate.convertAndSend(getExchange(), getRoutingKey(), msg);
+        log.info("exchange:{},routingKey:{},消息内容:{}", getExchange(), getRoutingKey(), msg);
     }
 
     public final void sendMessage(SourceMessageVO messageVO) {
         rabbitTemplate.convertAndSend(getExchange(), getRoutingKey(), messageVO);
+        log.info("exchange:{},routingKey:{},消息内容:{}", getExchange(), getRoutingKey(), messageVO);
     }
 }
