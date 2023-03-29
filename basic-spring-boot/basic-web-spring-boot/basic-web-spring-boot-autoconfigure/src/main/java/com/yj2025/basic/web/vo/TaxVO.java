a0672e24a978c4f3c9174fc7668fa5e052818db9
lucas <luz@yunji2025.com>
2023-03-29 17:19:40 +0800

    feat: 为税率值对象添加方法

--- basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/vo/TaxVO.java
+++ basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/vo/TaxVO.java
@@ -1,5 +1,6 @@
 package com.yj2025.basic.web.vo;
 
+import com.yj2025.basic.dao.value.object.TaxValueObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -12,4 +13,19 @@ public class TaxVO {
     private boolean taxIncluded;
     @Schema(description = "税率")
     private BigDecimal taxRate;
+
+    public TaxVO() {
+    }
+
+    public TaxVO(boolean taxIncluded, BigDecimal taxRate) {
+        this.taxIncluded = taxIncluded;
+        this.taxRate = taxRate;
+    }
+
+    public static TaxVO of(TaxValueObject tax) {
+        if (null == tax) {
+            return null;
+        }
+        return new TaxVO(tax.isTaxIncluded(), tax.getTaxRate());
+    }
 }
