b1c0b1728c4947bede08a7a60542be7973fed394
cofey <zhangbk@yj2025.com>
2023-04-06 19:55:14 +0800

    -- 加VO

--- basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/vo/SubmitVO.java
+++ basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/vo/SubmitVO.java
@@ -1,5 +1,6 @@
 package com.yj2025.basic.web.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -12,5 +13,6 @@ public class SubmitVO {
     @Schema(description = "提交人名称")
     private String submitName;
     @Schema(description = "提交时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     protected Date submitTime;
 }
