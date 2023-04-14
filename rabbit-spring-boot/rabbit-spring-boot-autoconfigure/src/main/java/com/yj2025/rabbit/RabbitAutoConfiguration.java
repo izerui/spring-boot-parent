895b1358b859dd185a477c9e5de0a026fb4d6c10
serv <liuyuhua69@gmail.com>
2023-04-14 21:48:28 +0800

    objectmapper primary

--- rabbit-spring-boot/rabbit-spring-boot-autoconfigure/src/main/java/com/yj2025/rabbit/RabbitAutoConfiguration.java
+++ rabbit-spring-boot/rabbit-spring-boot-autoconfigure/src/main/java/com/yj2025/rabbit/RabbitAutoConfiguration.java
@@ -26,6 +26,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class RabbitAutoConfiguration {
 
+    @Primary
     @Bean
     @ConditionalOnMissingBean
     public ObjectMapper objectMapper() {
