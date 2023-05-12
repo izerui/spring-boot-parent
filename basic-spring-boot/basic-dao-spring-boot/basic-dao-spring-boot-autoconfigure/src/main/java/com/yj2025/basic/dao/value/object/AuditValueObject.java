7ff3a9c3a774de75928972f64924580c4b0cac89
cofey <zhangbk@yj2025.com>
2023-05-12 18:33:13 +0800

    修改字段长度

--- basic-spring-boot/basic-dao-spring-boot/basic-dao-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dao/value/object/AuditValueObject.java
+++ basic-spring-boot/basic-dao-spring-boot/basic-dao-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/dao/value/object/AuditValueObject.java
@@ -9,7 +9,7 @@ import java.util.Date;
 @Data
 @Embeddable
 public class AuditValueObject {
-    @Column(name = "audit_remark", columnDefinition = "VARCHAR(64) COMMENT '审核/驳回说明'")
+    @Column(name = "audit_remark", columnDefinition = "VARCHAR(240) COMMENT '审核/驳回说明'")
     private String auditRemark;
     @Column(name = "auditor", columnDefinition = "VARCHAR(64) COMMENT '审核/驳回人CODE'")
     private String auditor;
