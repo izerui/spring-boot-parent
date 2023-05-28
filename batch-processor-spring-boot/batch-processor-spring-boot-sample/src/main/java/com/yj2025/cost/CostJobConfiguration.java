cb2acfb67ad4b27fa8097bba0890b3da5511b15a
serv <liuyuhua69@gmail.com>
2023-05-28 18:44:09 +0800

    自制件物料的使用量

--- batch-processor-spring-boot/batch-processor-spring-boot-sample/src/main/java/com/yj2025/cost/CostJobConfiguration.java
+++ batch-processor-spring-boot/batch-processor-spring-boot-sample/src/main/java/com/yj2025/cost/CostJobConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.listener.ChunkListenerSupport;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -24,13 +25,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
 
 @Slf4j
 @Configuration
 public class CostJobConfiguration {
 
-    private final static Function<String, String> MADE_FINISHED_TABLE = yearMonth -> String.format("made_finished_%s", yearMonth);
     @Autowired
     private JobBuilderFactory jobs;
     @Autowired
@@ -66,14 +65,15 @@ public class CostJobConfiguration {
     @Bean
     @StepScope
     public JdbcCursorItemReader<Map<String, Object>> step0Reader(@Value("#{stepExecution}") StepExecution stepExecution) {
-        String entCode = stepExecution.getJobParameters().getString("entCode");
-        String yearMonth = stepExecution.getJobParameters().getString("yearMonth");
+        final String entCode = stepExecution.getJobParameters().getString("entCode");
+        final String yearMonth = stepExecution.getJobParameters().getString("yearMonth");
         String sql = """
                 SELECT
                     r.ent_code,
                     '${yearMonth}' as ym,
-                	r.bom_id,
+                	r.demand_id,
                 	(SELECT x.inventory_id from manufacture.production_demand x where x.bom_id = r.bom_id limit 1) as inventory_id,
+                	(SELECT x.quantity from manufacture.production_demand x where x.bom_id = r.bom_id limit 1) as production_quantity,
                 	'1' as attribute_code,
                 	sum( r.quantity ) as quantity
                 FROM
@@ -104,11 +104,9 @@ public class CostJobConfiguration {
     @Bean
     @StepScope
     public JdbcBatchItemWriter<Map<String, Object>> step0Writer(@Value("#{stepExecution}") StepExecution stepExecution) {
-        String tempTableName = MADE_FINISHED_TABLE.apply(stepExecution.getJobParameters().getString("yearMonth"));
         String insertSQL = """
-                insert into %s (ent_code, bom_id, inventory_id, attribute_code, ym, quantity) values (:ent_code, :bom_id, :inventory_id, :attribute_code, :ym, :quantity)
+                insert into manufacture.year_month_made_finished (ent_code, demand_id, inventory_id, attribute_code, ym, quantity, production_quantity) values (:ent_code, :demand_id, :inventory_id, :attribute_code, :ym, :quantity, :production_quantity)
                 """;
-        insertSQL = String.format(insertSQL, tempTableName);
         JdbcBatchItemWriter<Map<String, Object>> batchItemWriter = new JdbcBatchItemWriterBuilder<Map<String, Object>>()
                 .dataSource(dataSource)
                 .columnMapped()
@@ -122,27 +120,10 @@ public class CostJobConfiguration {
     public StepExecutionListener step0ExeListener(@Value("#{stepExecution}") StepExecution stepExecution) {
         final String entCode = stepExecution.getJobParameters().getString("entCode");
         final String yearMonth = stepExecution.getJobParameters().getString("yearMonth");
-        final String tableName = MADE_FINISHED_TABLE.apply(yearMonth);
-        final String createTableIfNotExistsSQL = String.format(
-                """
-                        CREATE TABLE if not exists `%s` (
-                          `ent_code` varchar(128) NOT NULL COMMENT 'ent_code',
-                          `bom_id` varchar(128) NOT NULL COMMENT 'bom_id',
-                          `inventory_id` varchar(128) DEFAULT NULL COMMENT 'inventory_id',
-                          `ym` bigint DEFAULT NULL COMMENT '年月',
-                          `attribute_code` varchar(64) DEFAULT NULL COMMENT '货品属性',
-                          `quantity` decimal(24,8) DEFAULT '0.00000000',
-                          PRIMARY KEY (`bom_id`)
-                        ) ENGINE=InnoDB;
-                        """,
-                tableName
-
-        );
         return new StepExecutionListener() {
             @Override
             public void beforeStep(StepExecution stepExecution) {
-                jdbcTemplate.execute(createTableIfNotExistsSQL);
-                jdbcTemplate.execute(String.format("delete from %s where ent_code = '%s'", tableName, entCode));
+                jdbcTemplate.execute(String.format("delete from manufacture.year_month_made_finished where ent_code = '%s' and ym = '%s'", entCode, yearMonth));
             }
 
             @Override
@@ -164,17 +145,87 @@ public class CostJobConfiguration {
         };
     }
 
-    // 查询当前月份有过登数的自制件数量
     @Bean
     @JobScope
     public Step step1(@Value("#{jobExecution}") JobExecution jobExecution) {
-        return steps.get("从生产获取自制件的当月登数数据")
-                .listener(step0ExeListener(null))
+        return steps.get("获取当月使用的自制件物料的量")
+                .listener(step1ExeListener(null))
                 .<Map<String, Object>, Map<String, Object>>chunk(10000)
-                .reader(step0Reader(null))
-                .writer(step0Writer(null))
+                .reader(step1Reader(null))
+                .writer(step1Writer(null))
                 .listener(printChunkListener())
                 .build();
     }
+
+    @Bean
+    @StepScope
+    public StepExecutionListener step1ExeListener(@Value("#{stepExecution}") StepExecution stepExecution) {
+        final String entCode = stepExecution.getJobParameters().getString("entCode");
+        final String yearMonth = stepExecution.getJobParameters().getString("yearMonth");
+        return new StepExecutionListener() {
+            @Override
+            public void beforeStep(StepExecution stepExecution) {
+                jdbcTemplate.execute(String.format("delete from manufacture.year_month_material_used where ent_code = '%s' and ym = '%s'", entCode, yearMonth));
+            }
+
+            @Override
+            public ExitStatus afterStep(StepExecution stepExecution) {
+                return ExitStatus.COMPLETED;
+            }
+        };
+    }
+
+    @Bean
+    @StepScope
+    public JdbcCursorItemReader<Map<String, Object>> step1Reader(@Value("#{stepExecution}") StepExecution stepExecution) {
+        final String entCode = stepExecution.getJobParameters().getString("entCode");
+        final String yearMonth = stepExecution.getJobParameters().getString("yearMonth");
+        String sql = """
+                SELECT
+                    m.production_demand_id as demand_id,
+                    m.inventory_id,
+                    m.attribute_code,
+                    m.production_quantity,
+                    ((d.quantity / d.production_quantity) * m.production_quantity) as quantity,
+                    '${yearMonth}' as ym,
+                    m.ent_code
+                  FROM
+                    manufacture.production_demand_material m
+                    LEFT JOIN manufacture.year_month_made_finished d ON d.ent_code = m.ent_code
+                    AND d.demand_id = m.production_demand_id
+                  WHERE
+                    d.ent_code = '${entCode}'
+                    AND d.ym = '${yearMonth}'
+                    AND m.record_status = 1
+                    AND m.production_quantity > 0
+                    AND m.record_status = 1;
+                """;
+        sql = new StringSubstitutor(new HashMap<>() {{
+            put("entCode", entCode);
+            put("yearMonth", yearMonth);
+        }}).replace(sql);
+        return new JdbcCursorItemReaderBuilder<Map<String, Object>>()
+                .name("获取本月登数demand的物料消耗量")
+                .dataSource(dataSource)
+                .name("monthMadeInventoriesReader")
+                .sql(sql)
+                .rowMapper(new ColumnMapRowMapper())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public JdbcBatchItemWriter<Map<String, Object>> step1Writer(@Value("#{stepExecution}") StepExecution stepExecution) {
+        String insertSQL = """
+                insert into manufacture.year_month_material_used (ent_code, demand_id, inventory_id, attribute_code, ym, quantity, production_quantity) values (:ent_code, :demand_id, :inventory_id, :attribute_code, :ym, :quantity, :production_quantity)
+                """;
+        JdbcBatchItemWriter<Map<String, Object>> batchItemWriter = new JdbcBatchItemWriterBuilder<Map<String, Object>>()
+                .dataSource(dataSource)
+                .columnMapped()
+                .sql(insertSQL)
+                .build();
+        return batchItemWriter;
+    }
+
 
 }
