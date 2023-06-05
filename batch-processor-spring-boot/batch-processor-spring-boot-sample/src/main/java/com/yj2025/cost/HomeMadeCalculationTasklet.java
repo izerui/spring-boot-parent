dc6263db338dca1eb04a92bc324a66c3121ff53e
cofey <zhangbk@yj2025.com>
2023-06-05 20:51:50 +0800

    --

--- batch-processor-spring-boot/batch-processor-spring-boot-sample/src/main/java/com/yj2025/cost/HomeMadeCalculationTasklet.java
+++ batch-processor-spring-boot/batch-processor-spring-boot-sample/src/main/java/com/yj2025/cost/HomeMadeCalculationTasklet.java
@@ -17,6 +17,7 @@ import tech.tablesaw.api.StringColumn;
 import tech.tablesaw.api.Table;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -42,8 +43,6 @@ public class HomeMadeCalculationTasklet implements Tasklet {
     private transient Table homeMades;
     // 自制件的物料列表
     private transient Table materials;
-//    // 期初物料单价
-//    private transient Set<InventoryPriceVO> initInventoryPrices;
 
     public HomeMadeCalculationTasklet(StepExecution stepExecution, JdbcTemplate jdbcTemplate) {
         this.stepExecution = stepExecution;
@@ -57,7 +56,6 @@ public class HomeMadeCalculationTasklet implements Tasklet {
     private void initData() {
         initHomeMade();
         initMaterials();
-//        initPrices();
     }
 
     /**
@@ -68,7 +66,16 @@ public class HomeMadeCalculationTasklet implements Tasklet {
         String sql0 = String.format("select * from manufacture.year_month_made_finished where ent_code = '%s' and ym = '%s' order by level_label desc", entCode, yearMonth);
         homeMades = jdbcTemplate.query(sql0, (ResultSetExtractor<Table>) rs -> Table.read().db(rs));
         // 自制件列表信息增加待计算成本列
-        homeMades.addColumns(DoubleColumn.create("amount"));
+        homeMades.addColumns(DoubleColumn.create("material_cost"));
+        homeMades.addColumns(DoubleColumn.create("manpower_cost"));
+        homeMades.addColumns(DoubleColumn.create("manufacture_cost"));
+        homeMades.addColumns(DoubleColumn.create("total_cost"));
+        homeMades.addColumns(DoubleColumn.create("init_total_cost"));
+        homeMades.addColumns(DoubleColumn.create("init_total_quantity"));
+        homeMades.doubleColumn("manpower_cost").fillWith(RandomUtils.nextDouble(0.0, 100.00));
+        homeMades.doubleColumn("manufacture_cost").fillWith(RandomUtils.nextDouble(0.0, 100.00));
+        homeMades.doubleColumn("init_total_cost").fillWith(RandomUtils.nextDouble(0.0, 100.00));
+        homeMades.doubleColumn("init_total_quantity").fillWith(RandomUtils.nextDouble(0.0, 100.00));
         Assert.state(homeMades.containsColumn("inventory_id"), "自制件列表必须有[inventory_id]列");
         Assert.state(homeMades.containsColumn("quantity"), "自制件列表必须有[quantity]列");
         Assert.state(homeMades.containsColumn("demand_id"), "物料列表必须有[demand_id]列");
@@ -81,8 +88,6 @@ public class HomeMadeCalculationTasklet implements Tasklet {
         // 获取自制件对应的当月物料领用列表信息
         String sql1 = String.format("select * from manufacture.year_month_material_used where ent_code = '%s' and ym = '%s'", entCode, yearMonth);
         materials = jdbcTemplate.query(sql1, (ResultSetExtractor<Table>) rs -> Table.read().db(rs));
-        // 增加期初单价列
-        materials.addColumns(DoubleColumn.create("init_price"));
         // 增加最终单价列，最终计算成本使用本列
         materials.addColumns(DoubleColumn.create("price"));
         // 本月发生登数的自制件货品ID集合
@@ -95,10 +100,8 @@ public class HomeMadeCalculationTasklet implements Tasklet {
             Double initPrice = RandomUtils.nextDouble(0.0, 500.0);
             // TODO :end
             if (attributeCode != "1") {
-                row.setDouble("init_price", initPrice);
                 row.setDouble("price", initPrice);
             } else {
-                row.setDouble("init_price", 0.0);
                 // 本月未发生登数的自制物料直接使用期初单价
                 if (!madeInventoryIdSets.contains(inventoryId)) {
                     row.setDouble("price", initPrice);
@@ -108,27 +111,8 @@ public class HomeMadeCalculationTasklet implements Tasklet {
         Assert.state(materials.containsColumn("inventory_id"), "物料列表必须有[inventory_id]列");
         Assert.state(materials.containsColumn("attribute_code"), "物料列表必须有[attribute_code]列");
         Assert.state(materials.containsColumn("quantity"), "物料列表必须有[quantity]列");
-        Assert.state(materials.containsColumn("init_price"), "物料列表必须有[init_price]列");
         Assert.state(materials.containsColumn("demand_id"), "物料列表必须有[demand_id]列");
     }
-
-//    // 初始化期初货品单价信息
-//    private void initPrices() {
-//        this.initInventoryPrices = getInitPrice(entCode, materials.stringColumn("inventory_id").asSet());
-//    }
-
-//    // TODO 获取期初价格信息
-//    private Set<InventoryPriceVO> getInitPrice(String entCode, Set<String> inventoryIds) {
-//        return inventoryIds.stream().map(s -> {
-//            InventoryPriceVO priceVO = new InventoryPriceVO();
-//            priceVO.setInventoryId(s);
-//            priceVO.setEntCode(entCode);
-//            priceVO.setQuantity(new BigDecimal(RandomUtils.nextDouble(1.0, 50.0)));
-//            priceVO.setTotalAmount(new BigDecimal(RandomUtils.nextDouble(1.0, 500.0)));
-//            Assert.notNull(priceVO.getPrice(), "单价不能为NULL");
-//            return priceVO;
-//        }).collect(Collectors.toSet());
-//    }
 
     /**
      * 如果自制件没有期初单价，并且本月未发生登数, 则抛出异常
@@ -137,19 +121,19 @@ public class HomeMadeCalculationTasklet implements Tasklet {
         Set<String> unPricedInventoryIds = materials
                 .stringColumn("inventory_id")
                 .where(materials.stringColumn("attribute_code").isEqualTo("1"))
-                .where(materials.doubleColumn("init_price").isMissing())
+                .where(materials.doubleColumn("price").isMissing())
                 .asSet();
         StringColumn column = homeMades.stringColumn("inventory_id");
         column = column.where(column.isIn(unPricedInventoryIds));
         Sets.SetView<String> difference = Sets.difference(unPricedInventoryIds, column.asSet());
-        Assert.state(difference.size() == 0, "物料表中没有单价的自制件，在本月中未发现生产登数,请补充期初单价。相关货品ID:" + difference.toString());
+        Assert.state(difference.size() == 0, "物料表中没有单价的自制件，在本月中未发现生产登数,请补充期初单价。相关货品ID:" + difference);
     }
 
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         // 循环自制件的每行进行成本计算并得出其成本
         homeMades.forEach(row -> {
             // 如果当前自制件未计算成本
-            if (row.isMissing("amount")) {
+            if (row.isMissing("total_cost")) {
                 String demandId = row.getString("demand_id");
                 // 构造新的当前自制件的物料列表
                 Table materialTable = materials.where(materials.stringColumn("demand_id").isEqualTo(demandId));
@@ -157,52 +141,51 @@ public class HomeMadeCalculationTasklet implements Tasklet {
                 if (materialTable.doubleColumn("price").countMissing() > 0) {
                     return;
                 }
-                DoubleColumn quantityCol = materialTable.doubleColumn("quantity");
+                DoubleColumn quantityCol = materialTable.doubleColumn("quantity");// 实际使用数量
                 DoubleColumn priceCol = materialTable.doubleColumn("price");
-                DoubleColumn amoutCol = quantityCol.multiply(priceCol);
-                row.setDouble("amount", amoutCol.sum());
+                DoubleColumn amountCol = quantityCol.multiply(priceCol);
+                row.setDouble("material_cost", amountCol.sum());
+                double totalCost = row.getDouble("manpower_cost") + row.getDouble("manufacture_cost") + row.getDouble("material_cost");
+                row.setDouble("total_cost", totalCost);
             }
         });
 
         // 针对自制件列表按货品id分组，将所有算过成本的算出单价
         Table inventoryTable = homeMades
-                .summarize("inventory_id", "amount", "quantity", AggregateFunctions.countNonMissing, AggregateFunctions.sum)
+                .summarize(List.of("inventory_id", "total_cost", "quantity", "init_total_cost", "init_total_quantity"), AggregateFunctions.countNonMissing, AggregateFunctions.sum, AggregateFunctions.first)
                 .by("inventory_id");
         // 找到已经计算完成本的货品列表
-        Table amountCompletedTable = inventoryTable
-                .where(inventoryTable.doubleColumn("Count [amount]").isEqualTo(inventoryTable.doubleColumn("Count [inventory_id]")));
+        Table completedTable = inventoryTable
+                .where(inventoryTable.doubleColumn("Count [total_cost]").isEqualTo(inventoryTable.doubleColumn("Count [inventory_id]")));
         // 把已计算完的货品列表计算出单价
-        DoubleColumn priceColumn = amountCompletedTable.doubleColumn("Sum [amount]")
-                .divide(amountCompletedTable.doubleColumn("Sum [quantity]"))
-                .setName("price");
-        amountCompletedTable.addColumns(priceColumn);
+        DoubleColumn finalCost = completedTable.doubleColumn("Sum [total_cost]").add(completedTable.doubleColumn("First [init_total_cost]"));
+        DoubleColumn finalQuantity = completedTable.doubleColumn("Sum [quantity]").add(completedTable.doubleColumn("First [init_total_quantity]"));
+        DoubleColumn priceColumn = finalCost.divide(finalQuantity).setName("price");
+        completedTable.addColumns(priceColumn);
         // 最终结果: 半成品加权平均后的单价
         Map<String, Double> invPriceMap = new HashMap<>();
-        amountCompletedTable.forEach(row -> {
-            invPriceMap.put(row.getString("inventory_id"), row.getDouble("price"));
-        });
+        completedTable.forEach(row -> invPriceMap.put(row.getString("inventory_id"), row.getDouble("price")));
 
         // 将单价跟物料表的期初价进行加权平均后，写入price列
         materials.forEach(row -> {
             String inventoryId = row.getString("inventory_id");
-            Double initPrice = row.getDouble("init_price");
             if (invPriceMap.containsKey(inventoryId)) {
-                row.setDouble("price", (invPriceMap.get(inventoryId) + initPrice) / 2);
+                row.setDouble("price", invPriceMap.get(inventoryId));
             }
         });
 
         // 如果自制件列表有未算完成本的行，则继续循环，否则将结果返回
-        if (homeMades.column("amount").countMissing() > 0) {
+        if (homeMades.column("total_cost").countMissing() > 0) {
             return RepeatStatus.CONTINUABLE;
         } else {
             // 自制件每行计算自身的财务单价
-            DoubleColumn priceCol = homeMades.doubleColumn("amount")
+            DoubleColumn priceCol = homeMades.doubleColumn("total_cost")
                     .divide(homeMades.doubleColumn("quantity"))
                     .setName("price");
             homeMades.addColumns(priceCol);
             System.out.println(homeMades);
             System.out.println(materials);
-            System.out.println(homeMades.print());
+            System.out.println(completedTable);
             // TODO 写入结果
             return RepeatStatus.FINISHED;
         }
