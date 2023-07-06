27bba1e483efb5fa4c89258e57849f338cea8124
serv <liuyuhua69@gmail.com>
2023-07-06 21:23:13 +0800

    init

--- data-jdbc-spring-boot/data-jdbc-spring-boot-autoconfigure/src/main/java/com/yj2025/jdbc/tenant/TenantThreadLocal.java
+++ data-jdbc-spring-boot/data-jdbc-spring-boot-autoconfigure/src/main/java/com/yj2025/jdbc/tenant/TenantThreadLocal.java
@@ -7,7 +7,7 @@ import java.lang.annotation.*;
 /**
  * @author liuyuhua
  */
-@Description("将租户信息放入本地ThreadLocal中")
+@Description("将租户信息放入本地ThreadLocal中,在Entity中声明@Table(\"#{@tenantSharding.getTable('test_user')}\")即可实现根据租户动态分表")
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
