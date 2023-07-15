4d10e6e1c771854417eca68a4a793dae87c5641d
serv <liuyuhua69@gmail.com>
2023-07-15 19:17:20 +0800

    replacedLowerUnderscoreKey

--- data-jdbc-spring-boot/data-jdbc-spring-boot-autoconfigure/src/main/java/com/yj2025/jdbc/support/CriteriaUtils.java
+++ data-jdbc-spring-boot/data-jdbc-spring-boot-autoconfigure/src/main/java/com/yj2025/jdbc/support/CriteriaUtils.java
@@ -41,10 +41,7 @@ public final class CriteriaUtils {
                     return comparator.join(criteria, key, value);
                 }
             }
-        }
-        if (value == null) {
-            return criteria.and(CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, key)).isNull();
-        } else {
+        } else if (value != null) {
             return criteria.and(CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, key)).is(value);
         }
     }
