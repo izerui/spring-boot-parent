cdce3631ec07d149b747148813cfd65d9ba10ac4
serv <liuyuhua69@gmail.com>
2023-07-16 14:14:37 +0800

    共享锁、排它锁参考: https://docs.spring.io/spring-data/jdbc/docs/current/reference/html/#jdbc.locking

--- data-jdbc-spring-boot/data-jdbc-spring-boot-autoconfigure/src/main/java/com/yj2025/jdbc/PlatformJdbcRepository.java
+++ data-jdbc-spring-boot/data-jdbc-spring-boot-autoconfigure/src/main/java/com/yj2025/jdbc/PlatformJdbcRepository.java
@@ -1,30 +1,27 @@
 package com.yj2025.jdbc;
 
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.relational.core.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
 
 /**
  * @author liuyuhua
  * @date 2023年07月12日
  */
 @NoRepositoryBean
+@Transactional(readOnly = true)
 public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, PagingAndSortingRepository<T, ID>, QueryByExampleExecutor<T> {
-
-    int readTimeoutSeconds = 10;
 
     /**
      * 专用插入功能。如果聚合根是新的并进行插入，则跳过测试。
@@ -35,6 +32,7 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @param instance 要插入的聚合根实例。 不能为空
      * @return 返回保存后的实例.
      */
+    @Modifying
     T insert(T instance);
 
     /**
@@ -47,6 +45,7 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @return 返回保存后的实例.
      * @since 3.1
      */
+    @Modifying
     Iterable<T> insertAll(Iterable<T> instances);
 
     /**
@@ -55,6 +54,7 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @param instances     要插入的实例集合。 不能为空
      * @param generatedKeys 自动生成的列
      */
+    @Modifying
     void batchInsert(Collection<T> instances, String... generatedKeys);
 
     /**
@@ -64,6 +64,7 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @param batchValues value数组
      * @param <U>         任何对象或者map对象
      */
+    @Modifying
     <U> void batchUpdate(String namedSQL, Collection<U> batchValues);
 
     /**
@@ -73,6 +74,7 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @param batchValues Map数组
      * @param <U>         map对象数组
      */
+    @Modifying
     <U> void batchUpdate(String namedSQL, Map<String, ?>[] batchValues);
 
     /**
@@ -81,6 +83,7 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @param instance 要更新的聚合根实例. 不能为空
      * @return 返回更新后的实例
      */
+    @Modifying
     T update(T instance);
 
     /**
@@ -90,6 +93,7 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @return 返回更新后的实例
      * @since 3.1
      */
+    @Modifying
     Iterable<T> updateAll(Iterable<T> instances);
 
     /**
@@ -99,7 +103,6 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @return 数据库中存储的实例数量。不能为空
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     long count(Query query);
 
     /**
@@ -109,7 +112,6 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @return {@literal true} 如果对象存在.
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     boolean exists(Query query);
 
 
@@ -121,7 +123,6 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @throws org.springframework.dao.IncorrectResultSizeDataAccessException 如果找到多个.
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     Optional<T> findOne(Query query);
 
     /**
@@ -131,7 +132,6 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @return 包含所有匹配结果的非空排序列表。
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     Iterable<T> findAll(Query query);
 
     /**
@@ -143,7 +143,6 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @return 返回匹配给定 {@link Query} 条件的Page对象
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     Page<T> findAll(Query query, Pageable pageable);
 
     /**
@@ -153,7 +152,6 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @return 数据库中存储的实例数量。不能为空
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     long count(Map<String, Object> simpleMap);
 
     /**
@@ -163,7 +161,6 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @return {@literal true} 如果对象存在.
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     boolean exists(Map<String, Object> simpleMap);
 
     /**
@@ -174,7 +171,6 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @throws org.springframework.dao.IncorrectResultSizeDataAccessException 如果找到多个.
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     Optional<T> findOne(Map<String, Object> simpleMap);
 
     /**
@@ -185,7 +181,6 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @throws org.springframework.dao.IncorrectResultSizeDataAccessException 如果找到多个.
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     Optional<T> findOne(Map<String, Object> simpleMap, Sort sort);
 
     /**
@@ -195,7 +190,6 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @return 包含所有匹配结果的非空排序列表。
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     Iterable<T> findAll(Map<String, Object> simpleMap);
 
     /**
@@ -205,7 +199,6 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @return 包含所有匹配结果的非空排序列表。
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     Iterable<T> findAll(Map<String, Object> simpleMap, Sort sort);
 
     /**
@@ -217,75 +210,26 @@ public interface PlatformJdbcRepository<T, ID> extends CrudRepository<T, ID>, Pa
      * @return 返回匹配给定 {@link Query} 条件的Page对象
      * @since 3.0
      */
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     Page<T> findAll(Map<String, Object> simpleMap, Pageable pageable);
 
     @Deprecated(since = "3.1", forRemoval = true)
     @Override
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     Iterable<T> findAll();
-
-    /**
-     * 共享锁、排它锁参考: https://docs.spring.io/spring-data/jdbc/docs/current/reference/html/#jdbc.locking
-     * @param id must not be {@literal null}.
-     * @return
-     */
-    @Override
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
-    Optional<T> findById(ID id);
-
-    @Override
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
-    Iterable<T> findAllById(Iterable<ID> ids);
 
     @Deprecated(since = "3.1", forRemoval = true)
     @Override
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     Iterable<T> findAll(Sort sort);
 
     @Deprecated(since = "3.1", forRemoval = true)
     @Override
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     Page<T> findAll(Pageable pageable);
 
     @Deprecated(since = "3.1", forRemoval = true)
     @Override
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
     long count();
-
-    @Override
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
-    boolean existsById(ID id);
 
     @Deprecated(since = "3.1", forRemoval = true)
     @Override
     void deleteAll();
 
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
-    @Override
-    <S extends T> Optional<S> findOne(Example<S> example);
-
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
-    @Override
-    <S extends T> Iterable<S> findAll(Example<S> example);
-
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
-    @Override
-    <S extends T> Iterable<S> findAll(Example<S> example, Sort sort);
-
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
-    @Override
-    <S extends T> Page<S> findAll(Example<S> example, Pageable pageable);
-
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
-    @Override
-    <S extends T> long count(Example<S> example);
-
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
-    @Override
-    <S extends T> boolean exists(Example<S> example);
-
-    @Transactional(timeout = readTimeoutSeconds, readOnly = true)
-    @Override
-    <S extends T, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction);
 }
