6164f4a36d67dd2275343a461b7398cdead76ab0
陈军 <chenj@yunji2025.com>
2023-07-17 09:44:24 +0800

    单位文件

--- platform-commons/src/main/java/com/yj2025/commons/em/UnitEnum.java
+++ platform-commons/src/main/java/com/yj2025/commons/em/UnitEnum.java
@@ -3,11 +3,17 @@ package com.yj2025.commons.em;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
 import java.util.List;
+
+import static com.google.common.io.Resources.getResource;
 
 /**
  * Created by serv on 2017/6/22.
@@ -154,9 +160,22 @@ public final class UnitEnum {
                         return unitEnum;
                     }
                 }
+            }else {
+                InputStream in = UnitEnum.class.getResourceAsStream("/units");
+                if(in != null){
+                    List<String> lines = IOUtils.readLines(in, "UTF-8");
+                    for (String line : lines) {
+                        String[] split = line.split(",");
+                        if (split == null || split.length != 4) {
+                            throw new RuntimeException(line + " is readed error!");
+                        }
+                        UnitEnum unitEnum = new UnitEnum(split[0], split[1], split[2], Integer.parseInt(split[3]));
+                        units.add(unitEnum);
+                    }
+                }
             }
         } catch (Exception e) {
-            log.warn("/data/public/units " + e.getMessage());
+            log.error("单位文件找不到，初始化失败............../data/public/units " + e.getMessage());
         }
 
         throw new RuntimeException("计量单位【" + unitName + "】不在系统可用列表");
@@ -175,12 +194,24 @@ public final class UnitEnum {
                     UnitEnum unitEnum = new UnitEnum(split[0], split[1], split[2], Integer.parseInt(split[3]));
                     units.add(unitEnum);
                 }
+            }else {
+                InputStream in = UnitEnum.class.getResourceAsStream("/units");
+                if(in != null){
+                    List<String> lines = IOUtils.readLines(in, "UTF-8");
+                    for (String line : lines) {
+                        String[] split = line.split(",");
+                        if (split == null || split.length != 4) {
+                            throw new RuntimeException(line + " is readed error!");
+                        }
+                        UnitEnum unitEnum = new UnitEnum(split[0], split[1], split[2], Integer.parseInt(split[3]));
+                        units.add(unitEnum);
+                    }
+                }
             }
         } catch (Exception e) {
-            log.warn("/data/public/units " + e.getMessage());
+            log.error("单位文件找不到，初始化失败............../data/public/units " + e.getMessage());
         }
     }
 
     public final static List<UnitEnum> units = Lists.newArrayList();
-
 }
