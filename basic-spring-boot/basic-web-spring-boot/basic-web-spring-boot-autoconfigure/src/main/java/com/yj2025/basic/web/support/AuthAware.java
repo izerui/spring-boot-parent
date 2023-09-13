b36f9aa7b00d404e1a5dd31a50c992d9df54e1ec
lucas <luz@yunji2025.com>
2023-09-13 09:24:53 +0800

    feat: AuthAware不序列化

--- basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/support/AuthAware.java
+++ basic-spring-boot/basic-web-spring-boot/basic-web-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/web/support/AuthAware.java
@@ -1,31 +1,40 @@
 package com.yj2025.basic.web.support;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public interface AuthAware {
 
+    @JsonIgnore
     default String getEntCode() {
         return RequestHeaderHolder.getEntCode();
     }
 
+    @JsonIgnore
     default String getEntName() {
         return RequestHeaderHolder.getEntName();
     }
 
+    @JsonIgnore
     default String getUserCode() {
         return RequestHeaderHolder.getUserCode();
     }
 
+    @JsonIgnore
     default String getUserName() {
         return RequestHeaderHolder.getUserName();
     }
 
+    @JsonIgnore
     default String getAccountCode() {
         return RequestHeaderHolder.getAccountCode();
     }
 
+    @JsonIgnore
     default String getAccountName() {
         return RequestHeaderHolder.getAccountName();
     }
 
+    @JsonIgnore
     default String getPostCode() {
         return RequestHeaderHolder.getPostCode();
     }
