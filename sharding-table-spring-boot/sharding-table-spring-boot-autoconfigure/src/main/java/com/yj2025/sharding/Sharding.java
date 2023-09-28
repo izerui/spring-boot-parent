fa59dae5bb921fd96cac5093618d3e65d1ba7b65
serv <yifeng2000@126.com>
2023-09-28 22:34:35 +0800

    beforeDoExecute

--- sharding-table-spring-boot/sharding-table-spring-boot-autoconfigure/src/main/java/com/yj2025/sharding/Sharding.java
+++ sharding-table-spring-boot/sharding-table-spring-boot-autoconfigure/src/main/java/com/yj2025/sharding/Sharding.java
@@ -1,6 +1,7 @@
 package com.yj2025.sharding;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.util.StringUtils;
 
 public class Sharding extends AbstractTableSharding {
 
@@ -10,11 +11,11 @@ public class Sharding extends AbstractTableSharding {
 
     @Override
     protected String tableName(String sourceTable, String tenantId) {
-        return sourceTable.concat("_").concat(tenantId);
+        return sourceTable.concat("_").concat(StringUtils.replace(tenantId, "-", "_"));
     }
 
     @Override
     protected String tableName(String sourceTable, String tenantId, String year) {
-        return sourceTable.concat("_").concat(tenantId).concat("_").concat(year);
+        return sourceTable.concat("_").concat(StringUtils.replace(tenantId, "-", "_")).concat("_").concat(year);
     }
 }
