81cfc8a9720f134a1af31dc4c68f6664645d49ac
陈军 <chenj@yunji2025.com>
2023-10-23 15:01:02 +0800

    ZeroUtils

--- sequence-number-spring-boot/sequence-number-spring-boot-autoconfigure/src/main/java/com/yj2025/sequence/PeriodType.java
+++ sequence-number-spring-boot/sequence-number-spring-boot-autoconfigure/src/main/java/com/yj2025/sequence/PeriodType.java
@@ -3,47 +3,54 @@ package com.yj2025.sequence;
 import org.joda.time.DateTime;
 
 public enum PeriodType {
-    DAY {
+    DAY("yyyyMMdd") {
         @Override
         public Period period(DateTime dateTime) {
             return () -> dateTime.toString("yyyyMMdd");
         }
-    }, MONTH {
+
+    }, MONTH("yyyyMM") {
         @Override
         public Period period(DateTime dateTime) {
             return () -> dateTime.toString("yyyyMM");
         }
-    }, YEAR {
+    }, YEAR("yyyy") {
         @Override
         public Period period(DateTime dateTime) {
             return () -> dateTime.toString("yyyy");
         }
-    }, YEAR2 {
+    }, YEAR2("yy") {
         @Override
         public Period period(DateTime dateTime) {
             return () -> dateTime.toString("yy");
         }
-    }, YEAR_MONTH2 {
+    }, YEAR_MONTH2("yymm") {
         @Override
         public Period period(DateTime dateTime) {
             return () -> dateTime.toString("yymm");
         }
-    }, YEAR_WW2 {
+    }, YEAR_WW2("yyww") {
         @Override
         public Period period(DateTime dateTime) {
             return () -> dateTime.toString("yyww");
         }
-    }, DAY2 {
+    }, DAY2("yyMMdd") {
         @Override
         public Period period(DateTime dateTime) {
             return () -> dateTime.toString("yyMMdd");
         }
-    }, FOREVER {
+    }, FOREVER("FOREVER") {
         @Override
         public Period period(DateTime dateTime) {
             return () -> "FOREVER";
         }
     };
+
+    public String formateStr;
+
+    PeriodType(String formateStr) {
+        this.formateStr = formateStr;
+    }
 
     public abstract Period period(DateTime dateTime);
 
