0b062ba0ce1811cfdb58c3cac53954fb87fd15f0
serv <liuyuhua69@gmail.com>
2023-12-15 18:42:11 +0800

    camelToUnderscore

--- data-jdbc-spring-boot/data-jdbc-spring-boot-autoconfigure/src/main/java/com/yj2025/jdbc/support/CriteriaUtils.java
+++ data-jdbc-spring-boot/data-jdbc-spring-boot-autoconfigure/src/main/java/com/yj2025/jdbc/support/CriteriaUtils.java
@@ -7,7 +7,6 @@ import org.springframework.util.Assert;
 
 import java.util.Collection;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public final class CriteriaUtils {
