7e473ec9ea18aa1ce75e42d471fc047a7e7e993e
serv <liuyuhua69@gmail.com>
2024-01-08 14:47:39 +0800

    测试表创建语句

--- data-jdbc-spring-boot/data-jdbc-spring-boot-sample/src/test/java/com/yj2025/sample2/TestUserTest.java
+++ data-jdbc-spring-boot/data-jdbc-spring-boot-sample/src/test/java/com/yj2025/sample2/TestUserTest.java
@@ -2,6 +2,7 @@ package com.yj2025.sample2;
 
 import com.google.common.base.Stopwatch;
 import com.google.common.collect.Lists;
+import com.yj2025.basic.support.DbContext;
 import com.yj2025.jdbc.support.Comparator;
 import com.yj2025.sample2.entity.TestUser;
 import com.yj2025.sample2.mapping.GroupMapping;
@@ -249,7 +250,19 @@ public class TestUserTest {
 
 
     @Test
-    public void testFindByAge() {
+    public void testMapBatchInsert() {
+        Map map1 = new HashMap();
+        map1.put("ent_code", UUID.randomUUID().toString());
+        map1.put("root_bom_id", UUID.randomUUID().toString());
+        map1.put("bom_id", UUID.randomUUID().toString());
+        map1.put("alternative_material_changed", true);
+
+        Map map2 = new HashMap();
+        map2.put("ent_code", UUID.randomUUID().toString());
+        map2.put("root_bom_id", UUID.randomUUID().toString());
+        map2.put("bom_id", UUID.randomUUID().toString());
+        map2.put("alternative_material_changed", false);
+        DbContext.batchInsert("order_bom_item_638334323_2024", Lists.newArrayList(map1, map2), "id");
 //        System.out.println(byAge);
     }
 
