fd0e16f2c54a74fd06681e929141e0504c3848ee
serv <liuyuhua69@gmail.com>
2024-01-09 22:59:32 +0800

    1

--- basic-spring-boot/basic-infrastructure-spring-boot/basic-infrastructure-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/support/DbContext.java
+++ basic-spring-boot/basic-infrastructure-spring-boot/basic-infrastructure-spring-boot-autoconfigure/src/main/java/com/yj2025/basic/support/DbContext.java
@@ -270,6 +270,8 @@ public class DbContext {
             insert = new SimpleJdbcInsert(dataSource);
             insert.setTableName(tablename);
             // 当test库或者其他库有相同表的时候会出现错乱，特别是表名一致，但是字段不一致的问题。
+            insert.setCatalogName(getCatalog(dataSource));
+            // mysql 的schema其实就是数据库名
             insert.setSchemaName(getDatabase(dataSource));
             if (generatedKeys != null && generatedKeys.length > 0) {
                 insert.usingGeneratedKeyColumns(generatedKeys);
