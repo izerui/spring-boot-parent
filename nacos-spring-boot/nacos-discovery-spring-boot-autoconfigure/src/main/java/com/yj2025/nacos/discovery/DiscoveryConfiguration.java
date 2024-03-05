32a4acf5598a199f0c15949ead9407f16f11df08
serv <liuyuhua69@gmail.com>
2024-03-05 11:47:33 +0800

    nacos 服务发现，自动增加环境变量

--- nacos-spring-boot/nacos-discovery-spring-boot-autoconfigure/src/main/java/com/yj2025/nacos/discovery/DiscoveryConfiguration.java
+++ nacos-spring-boot/nacos-discovery-spring-boot-autoconfigure/src/main/java/com/yj2025/nacos/discovery/DiscoveryConfiguration.java
@@ -1,6 +1,9 @@
 package com.yj2025.nacos.discovery;
 
+import com.alibaba.cloud.nacos.ConditionalOnNacosDiscoveryEnabled;
 import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import org.springframework.boot.system.ApplicationHome;
+import org.springframework.boot.system.ApplicationPid;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,11 +15,22 @@ import java.util.Map;
 public class DiscoveryConfiguration {
 
     @Bean
+    @ConditionalOnNacosDiscoveryEnabled
     public NacosDiscoveryProperties nacosProperties() {
         NacosDiscoveryProperties nacosDiscoveryProperties = new NacosDiscoveryProperties();
         Map<String, String> metadata = nacosDiscoveryProperties.getMetadata();
-        metadata.put("startup.time", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-                .format(new Date()));
+        // 启动时间
+        metadata.put("startup.time",
+                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()));
+        // home path
+        metadata.put("boot.path", String.valueOf(new ApplicationHome(this.getClass()).getSource()));
+        // pid
+        metadata.put("boot.pid", new ApplicationPid().toString());
+        // app version
+        String appVersion = System.getenv("APP_VERSION");
+        if (appVersion != null && !"".equals(appVersion)) {
+            metadata.put("version", appVersion);
+        }
         return nacosDiscoveryProperties;
     }
 
