cd5c4ebe8054878f265df2c14253fef178816874
serv <liuyuhua69@gmail.com>
2024-03-16 16:36:10 +0800

    获取sharding-tables可以指定数据源及目标库来获取

--- sharding-table-spring-boot/sharding-table-spring-boot-autoconfigure/src/main/java/com/yj2025/sharding/Sharding.java
+++ sharding-table-spring-boot/sharding-table-spring-boot-autoconfigure/src/main/java/com/yj2025/sharding/Sharding.java
@@ -1,6 +1,5 @@
 package com.yj2025.sharding;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.util.StringUtils;
 
