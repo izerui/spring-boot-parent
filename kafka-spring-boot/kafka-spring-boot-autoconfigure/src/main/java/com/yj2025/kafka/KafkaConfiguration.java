c98bdbab8cac7dbdad4ba6b734780f859d59539a
serv <liuyuhua69@gmail.com>
2024-04-19 11:25:59 +0800

    去掉kafka事务

--- kafka-spring-boot/kafka-spring-boot-autoconfigure/src/main/java/com/yj2025/kafka/KafkaConfiguration.java
+++ kafka-spring-boot/kafka-spring-boot-autoconfigure/src/main/java/com/yj2025/kafka/KafkaConfiguration.java
@@ -58,16 +58,43 @@ public class KafkaConfiguration {
         return new MessageProducerImpl(kafkaTemplate, kafkaProperties, applicationName);
     }
 
-    /**
-     * 无需配置声明，设置kafka自身事务管理器的id前缀
-     * @return
-     */
-    @Bean
-    public DefaultKafkaProducerFactoryCustomizer factoryCustomizer() {
-        return producerFactory -> {
-            producerFactory.setTransactionIdPrefix("kafka-tx-");
-        };
-    }
+//    /**
+//     * 无需配置声明，设置kafka自身事务管理器的id前缀
+//     * @return
+//     */
+//    @Bean
+//    public DefaultKafkaProducerFactoryCustomizer factoryCustomizer() {
+//        return producerFactory -> {
+//            producerFactory.setTransactionIdPrefix("kafka-tx-");
+//        };
+//    }
+//
+//     /**
+//     * 如果没有jdbc事务再创建自身kafka事务管理器
+//     * @param producerFactory
+//     * @return
+//     */
+//    @Bean
+//	@ConditionalOnMissingBean(TransactionManager.class)
+//	public KafkaTransactionManager<?, ?> kafkaTransactionManager(ProducerFactory<?, ?> producerFactory) {
+//		return new KafkaTransactionManager<>(producerFactory);
+//	}
+//
+//    /**
+//     * 补偿机制： 防止重复创建事务管理器(可选)
+//     * 如果已经存在jdbc的事务管理器，则移除掉kafka自动创建的事务管理器
+//     * {@link #kafkaTransactionManager(ProducerFactory)}
+//     */
+//    @Bean
+//    public BeanDefinitionRegistryCustomizer kafkaTransactionManagerCustomizer() {
+//        return (registry, applicationContext) -> {
+//            if (registry.isBeanNameInUse("transactionManager")) {
+//                if (registry.isBeanNameInUse("kafkaTransactionManager")) {
+//                    registry.removeBeanDefinition("kafkaTransactionManager");
+//                }
+//            }
+//        };
+//    }
 
     @Bean
     public GlobalKafkaErrorHandler globalKafkaErrorHandler(MessageProducer messageProducer){
