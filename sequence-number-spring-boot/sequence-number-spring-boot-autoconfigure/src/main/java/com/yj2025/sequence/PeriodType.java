43690bffc45b9997bb177be70870beb44b3e2737
lucas <luz@yunji2025.com>
2024-09-09 14:47:55 +0800

    feat: 使用java8 date 替换joda

--- sequence-number-spring-boot/sequence-number-spring-boot-autoconfigure/src/main/java/com/yj2025/sequence/PeriodType.java
+++ sequence-number-spring-boot/sequence-number-spring-boot-autoconfigure/src/main/java/com/yj2025/sequence/PeriodType.java
@@ -1,47 +1,48 @@
 package com.yj2025.sequence;
 
-import org.joda.time.DateTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public enum PeriodType {
     DAY("yyyyMMdd") {
         @Override
-        public Period period(DateTime dateTime) {
-            return () -> dateTime.toString("yyyyMMdd");
+        public Period period(LocalDateTime dateTime) {
+            return () -> DateTimeFormatter.ofPattern("yyyyMMdd").format(dateTime);
         }
 
     }, MONTH("yyyyMM") {
         @Override
-        public Period period(DateTime dateTime) {
-            return () -> dateTime.toString("yyyyMM");
+        public Period period(LocalDateTime dateTime) {
+            return () -> DateTimeFormatter.ofPattern("yyyyMM").format(dateTime);
         }
     }, YEAR("yyyy") {
         @Override
-        public Period period(DateTime dateTime) {
-            return () -> dateTime.toString("yyyy");
+        public Period period(LocalDateTime dateTime) {
+            return () -> DateTimeFormatter.ofPattern("yyyy").format(dateTime);
         }
     }, YEAR2("yy") {
         @Override
-        public Period period(DateTime dateTime) {
-            return () -> dateTime.toString("yy");
+        public Period period(LocalDateTime dateTime) {
+            return () -> DateTimeFormatter.ofPattern("yy").format(dateTime);
         }
     }, YEAR_MONTH2("yymm") {
         @Override
-        public Period period(DateTime dateTime) {
-            return () -> dateTime.toString("yymm");
+        public Period period(LocalDateTime dateTime) {
+            return () -> DateTimeFormatter.ofPattern("yymm").format(dateTime);
         }
     }, YEAR_WW2("yyww") {
         @Override
-        public Period period(DateTime dateTime) {
-            return () -> dateTime.toString("yyww");
+        public Period period(LocalDateTime dateTime) {
+            return () -> DateTimeFormatter.ofPattern("yyww").format(dateTime);
         }
     }, DAY2("yyMMdd") {
         @Override
-        public Period period(DateTime dateTime) {
-            return () -> dateTime.toString("yyMMdd");
+        public Period period(LocalDateTime dateTime) {
+            return () -> DateTimeFormatter.ofPattern("yyMMdd").format(dateTime);
         }
     }, FOREVER("FOREVER") {
         @Override
-        public Period period(DateTime dateTime) {
+        public Period period(LocalDateTime dateTime) {
             return () -> "FOREVER";
         }
     };
@@ -52,7 +53,7 @@ public enum PeriodType {
         this.formateStr = formateStr;
     }
 
-    public abstract Period period(DateTime dateTime);
+    public abstract Period period(LocalDateTime dateTime);
 
     public interface Period {
         String getPeriodFormatter();
